3dc0b9a8fb8461595e9ef15732e97e470f3068f5
mi-song <misong023@khu.ac.kr>
2023-04-29 18:06:58 +0900

    [FIX] get_data.py

--- get_data.py
+++ get_data.py
@@ -39,9 +39,15 @@ for tag in tags:
 
 combined_gdf = gpd.GeoDataFrame(pd.concat(dataframes, ignore_index=True))
 
-whole_filepath = city_name + "_dataset/" + city_name + "_all_features.geojson"
+# 속성 값을 문자열로 변환합니다.
+for column in combined_gdf.columns:
+    if column != 'geometry':
+        combined_gdf[column] = combined_gdf[column].astype(str)
+
+whole_filepath = city_name + "_dataset/" + city_name + "_all_features2.geojson"
 
 combined_gdf.to_file(whole_filepath, driver="GeoJSON")
+
 
 # point / polygon data features
 
@@ -241,149 +247,3 @@ for i in range(1, len(polygons) + 1):
     building_filename = city_name + "_dataset/Buildings/" + city_name + f'_building{i}.geojson'
     intersections.to_file(building_filename, driver='GeoJSON')
 
-for i in range(1, len(polygons) + 1):
-    building_filename = city_name + "_dataset/Buildings/" + city_name + f'_building{i}.geojson'
-    with open(building_filename, "r", encoding='UTF8') as infile:
-        whole_geojson_data = json.load(infile)
-
-    for i in range(len(whole_geojson_data['features'])):
-        properties = whole_geojson_data['features'][i]['properties']
-        # building
-        if properties.get("building") != None:
-            properties["key"] = "residence"
-            if properties["building"] in ['civic']:
-                properties["key"] = "government_office"
-
-        # shop
-        if properties.get("shop") != None:
-            properties["key"] = "shop"
-            if properties["shop"] in ['convenience', 'supermarket']:
-                properties["key"] = "supermarket"
-            if properties["shop"] in ['herbalist', 'nutrition_supplements']:
-                properties["key"] = "alternative"
-
-        # amenity
-        if properties.get("amenity") != None:
-            if properties["amenity"] == 'marketplace':
-                properties["key"] = "supermarket"
-            if properties["amenity"] in ['restaurant', 'fast_food', 'cafe', 'bar', 'pub']:
-                properties["key"] = "restaurant"
-            if properties["amenity"] in ['kindergarten']:
-                properties["key"] = "kindergarten"
-            if properties["amenity"] in ['school']:
-                properties["key"] = "school"
-            if properties["amenity"] in ['college']:
-                properties["key"] = "college"
-            if properties["amenity"] in ['university']:
-                properties["key"] = "university"
-            if properties["amenity"] in ['police']:
-                properties["key"] = "police_station"
-            if properties["amenity"] in ['fire_station']:
-                properties["key"] = "fire_station"
-            if properties["amenity"] in ['bank']:
-                properties["key"] = "bank"
-            if properties["amenity"] in ['bureau_de_change']:
-                properties["key"] = "bureau_de_change"
-            if properties["amenity"] in ['court_house', 'townhall']:
-                properties["key"] = "government_office"
-            if properties["amenity"] in ['embassy']:
-                properties['key'] = 'embassy'
-            if properties["amenity"] in ['post_office']:
-                properties['key'] = 'post_office'
-            if properties["amenity"] in ['doctors']:
-                properties['key'] = 'doctor'
-            if properties["amenity"] in ['dentist']:
-                properties['key'] = 'dentist'
-            if properties["amenity"] in ['clinic']:
-                properties['key'] = 'clinic'
-            if properties["amenity"] in ['hospital']:
-                properties['key'] = 'hospital'
-            if properties["amenity"] in ['pharmacy']:
-                properties['key'] = 'pharmacy'
-            if properties["amenity"] in ['grave_yard']:
-                properties['key'] = 'cemetery'
-            if properties["amenity"] in ['place_of_worship']:
-                properties['key'] = 'place_of_worship'
-            if properties['amenity'] in ['community_centre']:
-                properties['key'] = 'community_centre'
-            if properties['amenity'] in ['library']:
-                properties['key'] = 'library'
-
-        # office
-        if properties.get("office") != None:
-            if properties["office"] in ['government']:
-                properties["key"] = 'government_office'
-
-        # tourism
-        if properties.get("tourism") != None:
-            properties["key"] = "tourism"
-            if properties["tourism"] in ['hotel', 'chalet', 'guest_house', 'hostel', 'motel']:
-                properties["key"] = "accommodation"
-
-        # government
-        if properties.get("government") != None:
-            properties["key"] = "government_office"
-
-        # militray
-        if properties.get("military") != None:
-            properties["key"] = "military"
-
-            # landuse
-        if properties.get("landuse") != None:
-            if properties["landuse"] in ['military']:
-                properties["key"] = "military"
-            if properties["landuse"] in ['cemetery']:
-                properties["key"] = "cemetery"
-            if properties["landuse"] in ['farmland', 'farmyard', 'greenhouse_horticulture']:
-                properties["key"] = "agriculture"
-            if properties["landuse"] in ['landfill']:
-                properties["key"] = "solid_waste"
-            if properties["landuse"] in ['forest']:
-                properties["key"] = "forest"
-            if properties["landuse"] in ['reservoir']:
-                properties["key"] = "reservoir"
-
-        # health_care
-        if properties.get("healthcare") != None:
-            if properties["healthcare"] in ['alternative']:
-                properties["key"] = "alternative"
-
-        # leisure
-        if properties.get("leisure") != None:
-            if properties["leisure"] in ['park']:
-                properties["key"] = "park"
-            if properties["leisure"] in ['stadium']:
-                properties["key"] = "stadium"
-            if properties["leisure"] in ['swimming_pool']:
-                properties["key"] = "swimming_pool"
-            if properties["leisure"] in ['pitch']:
-                properties["key"] = "pitch"
-            if properties["leisure"] in ['sport_centre']:
-                properties["key"] = "sport_centre"
-
-        # natural
-        if properties.get("natural") != None:
-            if properties["natural"] in ['water']:
-                properties["key"] = "water_body"
-            if properties["natural"] in ['grassland']:
-                properties["key"] = "grassland"
-            if properties["natural"] in ["wetland"]:
-                properties["key"] = "wetland"
-            if properties["natural"] in ["water"]:
-                properties["key"] = "reservoir"
-
-        # historic
-        if properties.get("historic") != None:
-            properties["key"] = "historic"
-
-        # water
-        if properties.get("water") != None:
-            if properties["water"] in ["reservoir"]:
-                properties["key"] = "reservoir"
-
-        # waterway
-        if properties.get("waterway") != None:
-            properties["key"] = "waterway"
-
-    with open(building_filename, "w") as f:
-        json.dump(whole_geojson_data, f)
