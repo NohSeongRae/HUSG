f7ec0bce49dd498aeeeb90d1dd155fb37f0a5c15
mi-song <misong023@khu.ac.kr>
2023-04-30 13:49:34 +0900

    [FIX] get_data.py (make folder, remove null, simplify)

--- get_data.py
+++ get_data.py
@@ -1,249 +1,188 @@
-# 모든 building data 다운로드
-
+import os
 import osmnx as ox
+import json
 import geopandas as gpd
-import pandas as pd
-from shapely.geometry import Polygon
 import networkx as nx
+import pandas as pd
+from shapely.geometry import shape, Point, LineString, Polygon
+
+city_name = ["atlanta"]
+location = ["Atlanta, United States"]
+
+for i in range(len(city_name)):
+    output_directories = [
+        city_name[i] + "_dataset",
+        city_name[i] + "_dataset/Boundaries",
+        city_name[i] + "_dataset/Buildings",
+    ]
 
-city_name = 'firenze'
-location = "Firenze, Italy"
+    for directory in output_directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-tags = {
-    "amenity": True,
-    "barrier": True,
-    "building": True,
-    "craft": True,
-    "emergency": True,
-    "geological": True,
-    "healthcare": True,
-    "historic": True,
-    "landuse": True,
-    "leisure": True,
-    "man_made": True,
-    "military": True,
-    "natural": True,
-    "office": True,
-    "place": True,
-    "shop": True,
-    "sport": True,
-    "tourism": True,
-    "water": True,
-    "waterway": True
-}
+    # 1 data download
 
-dataframes = []
-for tag in tags:
-    gdf = ox.geometries_from_place(location, {tag: tags[tag]})
-    dataframes.append(gdf)
+    tags = {
+        "amenity": True,
+        "building": True,
+        "craft": True,
+        "emergency": True,
+        "healthcare": True,
+        "historic": True,
+        "landuse": True,
+        "leisure": True,
+        "man_made": True,
+        "military": True,
+        "natural": True,
+        "office": True,
+        "place": True,
+        "shop": True,
+        "sport": True,
+        "tourism": True,
+        "water": True,
+        "waterway": True
+    }
 
-combined_gdf = gpd.GeoDataFrame(pd.concat(dataframes, ignore_index=True))
+    data = {"type": "FeatureCollection", "features": []}
 
-# 속성 값을 문자열로 변환합니다.
-for column in combined_gdf.columns:
-    if column != 'geometry':
-        combined_gdf[column] = combined_gdf[column].astype(str)
+    for tag in tags:
+        gdf = ox.geometries_from_place(location[i], {tag: tags[tag]})
+        geojson_data = json.loads(gdf.to_json())
 
-whole_filepath = city_name + "_dataset/" + city_name + "_all_features2.geojson"
+        for feature in geojson_data["features"]:
+            feature["properties"] = {k:v for k, v in feature["properties"].items() if v is not None}
+            data["features"].append(feature)
 
-combined_gdf.to_file(whole_filepath, driver="GeoJSON")
+    data_filepath = city_name[i] + "_dataset/" + city_name[i] + "_all_features.geojson"
 
+    with open(data_filepath, 'w') as f:
+        json.dump(data, f)
 
-# point / polygon data features
+    print("data download complete")
 
-gdf = gpd.read_file(whole_filepath)
+    # point / polygon 나누기
 
-point_filepath = city_name + "_dataset/" + city_name + "_point_data.geojson"
+    with open(data_filepath, "r", encoding='UTF-8') as file:
+        data_geojson = json.load(file)
 
-point_data = gdf[gdf.geometry.geom_type == "Point"]
-point_data.to_file(point_filepath, driver="GeoJSON")
+    point_features = {"type": "FeatureCollection", "features": []}
+    polygon_features = {"type": "FeatureCollection", "features": []}
 
-gdf = gpd.read_file(whole_filepath)
+    for feature in data_geojson["features"]:
+        geom_type = feature["geometry"]["type"]
 
-polygon_filepath = city_name + "_dataset/" + city_name + "_polygon_data.geojson"
+        if geom_type == "Point":
+            point_features["features"].append(feature)
+        elif geom_type == "Polygon":
+            polygon_features["features"].append(feature)
 
-polygon_data = gdf[gdf.geometry.geom_type == "Polygon"]
-polygon_data.to_file(polygon_filepath, driver="GeoJSON")
+    point_data_filepath = city_name[i] + "_dataset/" + city_name[i] + "_point_data.geojson"
+    with open(point_data_filepath, "w", encoding='UTF-8') as outfile:
+        json.dump(point_features, outfile)
 
-import json
+    polygon_data_filepath = city_name[i] + "_dataset/" + city_name[i] + "_polygon_data.geojson"
+    with open(polygon_data_filepath, "w", encoding='UTF-8') as outfile:
+        json.dump(polygon_features, outfile)
 
-# null feature 제거
+    print("extract point / polygon complete")
 
-with open(point_filepath, "r", encoding='UTF-8') as file:
-    geojson_data = json.load(file)
+    # 각 point 들이 어떤 polygon에 속하는지 (POI)
 
-for feature in geojson_data["features"]:
-    properties = feature["properties"]
-    to_delete = [key for key, value in properties.items() if value is None]
-    for key in to_delete:
-        del properties[key]
+    point_gdf = gpd.read_file(point_data_filepath)
+    points = []
+    for _, row in point_gdf.iterrows():
+        points.append(row['geometry'])
 
-point_filtered_filepath = city_name + "_dataset/" + city_name + "_point_data_filtered.geojson"
+    polygon_gdf = gpd.read_file(polygon_data_filepath)
+    polygons = []
+    for _, row in polygon_gdf.iterrows():
+        polygons.append(row['geometry'])
 
-with open(point_filtered_filepath, "w") as file:
-    json.dump(geojson_data, file)
+    index_mapping = {}
 
-with open(polygon_filepath, "r", encoding='UTF-8') as file:
-    geojson_data = json.load(file)
+    for idx, point in enumerate(points):
+        found = False
+        for polygon_idx, polygon in enumerate(polygons):
+            if point.within(polygon):
+                index_mapping[idx] = polygon_idx
+                found = True
+                break
 
-for feature in geojson_data["features"]:
-    properties = feature["properties"]
-    to_delete = [key for key, value in properties.items() if value is None]
-    for key in to_delete:
-        del properties[key]
+    polygon_filepath = city_name[i] + '_dataset/' + city_name[i] + '_polygon_data.geojson'
 
-polygon_filtered_filepath = city_name + "_dataset/" + city_name + "_polygon_data_filtered.geojson"
+    with open(polygon_filepath, 'r', encoding='UTF-8') as file:
+        polygon_json = json.load(file)
 
-with open(polygon_filtered_filepath, "w") as file:
-    json.dump(geojson_data, file)
+    point_filepath = city_name[i] + '_dataset/' + city_name[i] + '_point_data.geojson'
 
-# 각 point 들이 어느 polygon에 속하는지
+    with open(point_filepath, 'r', encoding='UTF-8') as file:
+        point_json = json.load(file)
 
-import json
-from shapely.geometry import Point
-import geopandas as gpd
+    max_index = len(polygon_json['features']) - 1
 
-point_gdf = gpd.read_file(point_filtered_filepath)
+    for source_index, target_index in index_mapping.items():
+        if target_index <= max_index:
+            properties_to_add = point_json['features'][source_index]['properties']
+            for key, value in properties_to_add.items():
+                if key not in polygon_json['features'][target_index]['properties']:
+                    polygon_json['features'][target_index]['properties'][key] = value
+        else:
+            print(f"Skipping index {target_index} because it's out of range.")
 
-points = []
+    combined_filepath = city_name[i] + '_dataset/' + city_name[i] + '_polygon_data_combined.geojson'
 
-for _, row in point_gdf.iterrows():
-    points.append(row['geometry'])
+    with open(combined_filepath, "w") as outfile:
+        json.dump(polygon_json, outfile)
 
-polygon_gdf = gpd.read_file(polygon_filtered_filepath)
+    print("POI 합치기 완료")
 
-polygons = []
+    custom_filter = '["highway"~"motorway|trunk|primary|secondary|tertiary|unclassified|residential|motorway_link|trunk_link|primary_link|secondary_link|tertiary_link|living_street"]'
+    graph = ox.graph_from_place(location[i], network_type="all", simplify=False, custom_filter=custom_filter)
 
-for _, row in polygon_gdf.iterrows():
-    polygons.append(row['geometry'])
+    simple_graph = nx.Graph(graph)
+    simple_graph = simple_graph.to_undirected()
 
-# index 판단?
+    cycles = nx.cycle_basis(simple_graph)
 
-index_mapping = {}
+    polygons = []
+    for cycle in cycles:
+        coords = []
+        for node in cycle:
+            node_data = simple_graph.nodes[node]
+            if 'x' in node_data and 'y' in node_data:
+                coords.append((node_data['x'], node_data['y']))
 
-for idx, point in enumerate(points):
-    found = False
-    for polygon_idx, polygon in enumerate(polygons):
-        if point.within(polygon):
-            index_mapping[idx] = polygon_idx
-            found = True
-            break
+        if len(coords) >= 3:
+            polygon = Polygon(coords)
+            polygons.append(polygon)
 
-with open(polygon_filtered_filepath, "r", encoding='UTF-8') as file:
-    polygon_json = json.load(file)
+    for j in range(len(polygons)):
+        gdf = gpd.GeoDataFrame(geometry=[polygons[i]])
+        polygon_filename = city_name[i] + '_dataset/Boundaries/' + city_name[i] + f'_boundaries{i+1}.geojson'
+        gdf.to_file(polygon_filename, driver='GeoJSON')
 
-with open(point_filtered_filepath, "r", encoding='UTF-8') as file:
-    point_json = json.load(file)
+    print("Boundary 추출 완료")
 
+    data_filepath = city_name[i] + '_dataset/' + city_name[i] + "_polygon_data_combined.geojson"
 
-max_index = len(polygon_json['features']) - 1
+    with open(data_filepath, "r", encoding='UTF-8') as file:
+        data_geojson = json.load(file)
 
-for source_index, target_index in index_mapping.items():
-    if target_index <= max_index:
-        properties_to_add = point_json['features'][source_index]['properties']
-        for key, value in properties_to_add.items():
-            if key not in polygon_json['features'][target_index]['properties']:
-                polygon_json['features'][target_index]['properties'][key] = value
-    else:
-        print(f"Skipping index {target_index} because it's out of range.")
+    for j in range(1, len(polygons)):
+        boundary_filename = city_name[i] + '_dataset/Boundaries/' + city_name[i] + f'_boundaries{i}.geojson'
 
-polygon_combined_filepath = city_name + "_dataset/" + city_name + "_polygon_data_combined.geojson"
+        with open(boundary_filename, "r", encoding='UTF-8') as file:
+            boundary_geojson = json.load(file)
 
-with open(polygon_combined_filepath, "w") as outfile:
-    json.dump(polygon_json, outfile)
+        boundary_polygon = shape(boundary_geojson["features"][0]["geometry"])
+        intersections = {"type": "FeatureCollection", "features": []}
 
-# 각 polygon boundary에 해당하는 data 추출
+        for feature in data_geojson["features"]:
+            geom = shape(feature["geometry"])
 
+            if geom.within(boundary_polygon):
+                intersections["features"].append(feature)
 
-data_filepath = city_name + "_dataset/" + city_name + "_all_features.geojson"
-
-data = gpd.read_file(data_filepath)
-
-# 기하학적 유형에 따라 데이터를 분리합니다.
-point_data = data[data.geometry.geom_type == 'Point']
-linestring_data = data[data.geometry.geom_type == 'LineString']
-polygon_data = data[data.geometry.geom_type == 'Polygon']
-
-for i in range(1, len(polygons) + 1):
-    polygon_filename = city_name + "_dataset/Boundaries/" + city_name + f'_boundaries{i}.geojson'
-    boundary = gpd.read_file(polygon_filename)
-
-    # CRS 일치 여부 확인
-    point_data = point_data.to_crs(boundary.crs)
-    linestring_data = linestring_data.to_crs(boundary.crs)
-    polygon_data = polygon_data.to_crs(boundary.crs)
-
-    # 각 기하학적 유형에 대해 overlay를 수행하고 결과를 병합합니다.
-    point_intersections = gpd.overlay(point_data, boundary, how='intersection')
-    linestring_intersections = gpd.overlay(linestring_data, boundary, how='intersection')
-    polygon_intersections = gpd.overlay(polygon_data, boundary, how='intersection')
-
-    intersections = gpd.GeoDataFrame(
-        pd.concat([point_intersections, linestring_intersections, polygon_intersections], ignore_index=True))
-
-    building_filename = city_name + "_dataset/Buildings/" + city_name +  f'_building{i}.geojson'
-    intersections.to_file(building_filename, driver='GeoJSON')
-
-
-
-# road network -> cycle -> polygon
-
-graph = ox.graph_from_place(location, network_type='all', simplify=False)
-
-simple_graph = nx.Graph(graph)
-simple_graph = simple_graph.to_undirected()
-
-cycles = nx.cycle_basis(simple_graph)
-
-# get cycle & polygon coordinates
-
-polygons = []
-for cycle in cycles:
-    coords = []
-    for node in cycle:
-        node_data = simple_graph.nodes[node]
-        if 'x' in node_data and 'y' in node_data:
-            coords.append((node_data['x'], node_data['y']))
-
-    if len(coords) >= 3:
-        polygon = Polygon(coords)
-        polygons.append(polygon)
-
-# polygon boundary data 저장
-
-for i in range(len(polygons)):
-    gdf = gpd.GeoDataFrame(geometry=[polygons[i]])
-    polygon_filename = city_name + "_dataset/Boundaries/" + city_name + f'_boundaries{i + 1}.geojson'
-    gdf.to_file(polygon_filename, driver='GeoJSON')
-
-# 각 polygon boundary에 해당하는 data 추출
-
-data_filepath = city_name + "_dataset/" + city_name + "_all_features.geojson"
-
-data = gpd.read_file(data_filepath)
-
-# 기하학적 유형에 따라 데이터를 분리합니다.
-point_data = data[data.geometry.geom_type == 'Point']
-linestring_data = data[data.geometry.geom_type == 'LineString']
-polygon_data = data[data.geometry.geom_type == 'Polygon']
-
-for i in range(1, len(polygons) + 1):
-    polygon_filename = city_name + "_dataset/Boundaries/" + city_name + f'_boundaries{i}.geojson'
-    boundary = gpd.read_file(polygon_filename)
-
-    # CRS 일치 여부 확인
-    point_data = point_data.to_crs(boundary.crs)
-    linestring_data = linestring_data.to_crs(boundary.crs)
-    polygon_data = polygon_data.to_crs(boundary.crs)
-
-    # 각 기하학적 유형에 대해 overlay를 수행하고 결과를 병합합니다.
-    point_intersections = gpd.overlay(point_data, boundary, how='intersection')
-    linestring_intersections = gpd.overlay(linestring_data, boundary, how='intersection')
-    polygon_intersections = gpd.overlay(polygon_data, boundary, how='intersection')
-
-    intersections = gpd.GeoDataFrame(
-        pd.concat([point_intersections, linestring_intersections, polygon_intersections], ignore_index=True))
-
-    building_filename = city_name + "_dataset/Buildings/" + city_name + f'_building{i}.geojson'
-    intersections.to_file(building_filename, driver='GeoJSON')
-
+        building_filename = city_name[i] + '_dataset/Buildings/' + city_name[i] + f'_buildings{i}.geojson'
+        with open(building_filename, 'w', encoding='UTF-8') as outfile:
+            json.dump(intersections, outfile)
