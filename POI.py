79584412933b2d53dcb4c1810467dedefa3e3c19
mi-song <misong023@khu.ac.kr>
2023-05-12 03:03:08 +0900

    [FIX] POI 수정완료

--- POI.py
+++ POI.py
@@ -1,50 +1,48 @@
-import json
 import geopandas as gpd
+import json
+from concurrent.futures import ThreadPoolExecutor
+from cityname import city_name
 
+polygon_filepath = city_name + '_dataset/' + city_name + '_polygon_data.geojson'
+point_filepath = city_name + '_dataset/' + city_name + '_point_data.geojson'
 
-def POI(city_name):
-    point_data_filepath = city_name + "_dataset/" + city_name + "_point_data.geojson"
-    polygon_data_filepath = city_name + "_dataset/" + city_name + "_polygon_data.geojson"
+polygons = gpd.read_file(polygon_filepath)
 
-    point_gdf = gpd.read_file(point_data_filepath)
-    points = []
-    for _, row in point_gdf.iterrows():
-        points.append(row['geometry'])
+points = gpd.read_file(point_filepath)
 
-    polygon_gdf = gpd.read_file(polygon_data_filepath)
-    polygons = []
-    for _, row in polygon_gdf.iterrows():
-        polygons.append(row['geometry'])
+index_mapping = {}
 
-    index_mapping = {}
+def process_point(point):
+    point_idx, point_geom = point
+    for i, polygon in polygons.iterrows():
+        if point_geom.within(polygon.geometry):
+            return point_idx, i
+    return point_idx, None
 
-    for idx, point in enumerate(points):
-        found = False
-        for polygon_idx, polygon in enumerate(polygons):
-            if point.within(polygon):
-                index_mapping[idx] = polygon_idx
-                found = True
-                break
+with ThreadPoolExecutor() as executor:
+    futures = [executor.submit(process_point, (i, point)) for i, point in points.geometry.items()]
 
-    polygon_filepath = city_name + '_dataset/' + city_name + '_polygon_data.geojson'
+    for future in futures:
+        point_idx, polygon_idx = future.result()
+        if polygon_idx is not None:
+            index_mapping[point_idx] = polygon_idx
 
-    with open(polygon_filepath, 'r', encoding='UTF-8') as file:
-        polygon_json = json.load(file)
-    point_filepath = city_name + '_dataset/' + city_name + '_point_data.geojson'
-    with open(point_filepath, 'r', encoding='UTF-8') as file:
-        point_json = json.load(file)
-    max_index = len(polygon_json['features']) - 1
-    for source_index, target_index in index_mapping.items():
-        if target_index <= max_index:
-            properties_to_add = point_json['features'][source_index]['properties']
-            for key, value in properties_to_add.items():
-                if key not in polygon_json['features'][target_index]['properties']:
-                    polygon_json['features'][target_index]['properties'][key] = value
-        else:
-            print(f"Skipping index {target_index} because it's out of range.")
-    combined_filepath = city_name + '_dataset/' + city_name + '_polygon_data_combined.geojson'
+polygon_filepath = city_name + '_dataset/' + city_name + '_polygon_data.geojson'
+with open(polygon_filepath, 'r', encoding='UTF-8') as file:
+    polygon_json = json.load(file)
+point_filepath = city_name + '_dataset/' + city_name + '_point_data.geojson'
+with open(point_filepath, 'r', encoding='UTF-8') as file:
+    point_json = json.load(file)
+max_index = len(polygon_json['features']) - 1
 
-    with open(combined_filepath, "w") as outfile:
-        json.dump(polygon_json, outfile)
-
-    print("POI 합치기 완료")
+for source_index, target_index in index_mapping.items():
+    if target_index <= max_index:
+        properties_to_add = point_json['features'][source_index]['properties']
+        for key, value in properties_to_add.items():
+            if key not in polygon_json['features'][target_index]['properties']:
+                polygon_json['features'][target_index]['properties'][key] = value
+    else:
+        print(f"Skipping index {target_index} because it's out of range.")
+combined_filepath = city_name + '_dataset/' + city_name + '_polygon_data_combined.geojson'
+with open(combined_filepath, "w") as outfile:
+    json.dump(polygon_json, outfile)
