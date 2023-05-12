e58c1c50532cc5cb96af8d71cb508e01be54837b
mi-song <misong023@khu.ac.kr>
2023-05-12 17:43:42 +0900

    [FIX] filepath

--- get_building.py
+++ get_building.py
@@ -1,6 +1,8 @@
 import os
 import geopandas as gpd
 import concurrent.futures
+import json
+
 
 def process_boundary(city_name, i, data_filepath):
     # boundary file 불러오기
@@ -8,21 +10,21 @@ def process_boundary(city_name, i, data_filepath):
     boundary_gdf = gpd.read_file(boundary_filename)
     data_gdf = gpd.read_file(data_filepath)
 
-    """
-    # boundary polygon과 building polygon의 공간 관계를 파악해 boundary polygon 내에 속하는 building polygon 찾기
-    # geopandas의 sjoin 함수는 r-tree를 기반으로 탐색을 수행
-    # how="left", predicate="within"은 왼쪽 param(chunk_points)가 오른쪽 param(polygons)에 속하는지(within)를 판단하는 것을 의미
-    :param chunk_points: POI chunk
-    :param polygons: building
-    :return: GeoDataFrame 형태로 point, polygon index 정보 반환 - 각각의 point가 어떤 index의 polygon에 속하는지
-     (columns: index(point), geometry(point), index_right(polygon index))
-    """
-
     intersections = gpd.sjoin(data_gdf, boundary_gdf, how="left", predicate='within')
 
     if not intersections.empty:  # building이 하나라도 존재하는 경우에만 저장
         building_filename = city_name + '_dataset/' + 'Buildings/' + city_name + f'_buildings{i}.geojson'
-        intersections.to_file(building_filename, driver='GeoJSON')
+
+        # Geopandas dataframe을 GeoJSON 형식으로 변환
+        geojson = json.loads(intersections.to_json())
+
+        # null 값을 가진 features 제거
+        geojson['features'] = [feature for feature in geojson['features'] if feature['properties'] is not None]
+
+        # GeoJSON 데이터를 파일로 저장
+        with open(building_filename, 'w') as f:
+            json.dump(geojson, f)
+
 
 def get_building(city_name):
     dir_path = city_name + '_dataset/Boundaries/'
@@ -35,6 +37,7 @@ def get_building(city_name):
         futures = []
         for i, _ in enumerate(range(1, filenum), start=1):
             futures.append(executor.submit(process_boundary, city_name, i, data_filepath))
+
 
 if __name__ == '__main__':
     get_building('littlerock')
