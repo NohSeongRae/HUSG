51a434adab95d97b308a8377e42f748a817d31ab
mi-song <misong023@khu.ac.kr>
2023-06-20 17:52:46 +0900

    [ADD] combine.py (겹치는 building data 합치기)

--- get_data/combine.py
+++ get_data/combine.py
@@ -1,59 +1,89 @@
 from shapely.geometry import Polygon
-import sys
+from shapely.wkt import loads, dumps
 import os
 import json
+import geojson
 
-current_script_path = os.path.dirname(os.path.abspath(__file__))
-husg_directory_path = os.path.dirname(current_script_path)
-sys.path.append(husg_directory_path)
-
-# from etc.cityname import city_name
-
-city_name = "littlerock"
-
-def check_intersection(polygons):
-    # Calculate area for each polygon
-    areas = {i: polygon.area for i, polygon in enumerate(polygons)}
-
-    # Sort polygons by area -> 오름차순
-    sorted_polygons = sorted(areas.items(), key=lambda item: item[1])
-
-    print(sorted_polygons)
-
-    intersection_polygons = []
-
-    for i in range(len(sorted_polygons)):
-        for j in range(i + 1, len(sorted_polygons)):
-            # If a smaller polygon intersects with a larger one
-            if polygons[sorted_polygons[i][0]].intersects(polygons[sorted_polygons[j][0]]):
-                # 작은 거 : 큰 거
-                print(f"Polygon {sorted_polygons[i][0]} intersects with Polygon {sorted_polygons[j][0]}")
-                intersection_polygons.append({sorted_polygons[i][0]: sorted_polygons[j][0]})
-
-    return intersection_polygons
+city_name = "singapore"
 
 dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Boundaries')
 files = os.listdir(dir_path)
 filenum = len(files)
 
+index = 0
+
 for i in range(1, filenum + 1):
-    if i == 12:
+    building_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
+                                     'Buildings', f'{city_name}_buildings{i}.geojson')
+    if os.path.exists(building_filename):
+        index += 1
+        with open(building_filename, "r", encoding='UTF-8') as file:
+            building_data = json.load(file)
+
+        if not building_data["features"]:
+            continue
+
+        colors = {}
+        polygons = []
+        keys = []
+
+        for feature in building_data["features"]:
+            geometry = feature["geometry"].get("coordinates")
+            key = feature["properties"].get("key")
+            polygon = Polygon(geometry[0])
+            polygons.append(polygon)
+            keys.append(key)
+
+        polygon_index_map = {dumps(p): i for i, p in enumerate(polygons)}
+
+        polygon_strings = set(polygon_index_map.keys())
+
+        polygons_unique = [loads(ps) for ps in polygon_strings]
+        indexes_unique = [polygon_index_map[ps] for ps in polygon_strings]
+
+        polygons = polygons_unique
+        combined_polygons = polygons_unique
+        combined_keys = []
+
+        for i in range(len(polygons)):
+            combined_keys.append(keys[indexes_unique[i]])
+
+        indexes_to_remove = []
+
+        for i in range(len(polygons)):
+            for j in range(i + 1, len(polygons)):
+                if polygons[i].contains(polygons[j]):
+                    indexes_to_remove.append(i)
+                    if combined_keys[j] == "residence":
+                        combined_keys[j] = combined_keys[i]
+                elif polygons[j].contains(polygons[i]):
+                    indexes_to_remove.append(j)
+                    if combined_keys[i] == "residence":
+                        combined_keys[i] = combined_keys[j]
+
+        indexes_to_remove = list(set(indexes_to_remove))
+        indexes_to_remove.sort(reverse=True)
+
+        for idx in indexes_to_remove:
+            del combined_keys[idx]
+            del combined_polygons[idx]
+
+        geojson_polygons = [geojson.Feature(geometry=polygon, properties={"key": key}) for polygon, key in zip(combined_polygons, combined_keys)]
+
+        feature_collection = geojson.FeatureCollection(geojson_polygons)
+
         building_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
-                                         'Buildings', f'{city_name}_buildings{i}.geojson')
+                                         'Combined_Buildings', f'{city_name}_buildings{index}.geojson')
 
+        with open(building_filename, 'w') as f:
+             json.dump(feature_collection, f)
 
-        if os.path.exists(building_filename):
-            with open(building_filename, "r", encoding='UTF-8') as file:
-                building_data = json.load(file)
+import shutil
 
-            colors = {}
-            polygons = []
+dir = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Buildings')
+if os.path.exists(dir):
+    shutil.rmtree(dir)
 
-            for feature in building_data["features"]:
-                geometry = feature["geometry"].get("coordinates")
-                polygon = Polygon(geometry[0])
-                polygons.append(polygon)
+temp_dir = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Combined_Buildings')
 
-            intersection_polygons = check_intersection(polygons)
-
-            print(intersection_polygons)
+os.rename(temp_dir, dir)
--- test.py
+++ test.py
@@ -2,56 +2,88 @@ from shapely.geometry import Polygon
 from shapely.wkt import loads, dumps
 import os
 import json
+import geojson
 
-city_name = "littlerock"
+city_name = "singapore"
 
 dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Boundaries')
 files = os.listdir(dir_path)
 filenum = len(files)
 
+index = 0
+
 for i in range(1, filenum + 1):
-    if i == 33:
+    building_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
+                                     'Buildings', f'{city_name}_buildings{i}.geojson')
+    if os.path.exists(building_filename):
+        index += 1
+        with open(building_filename, "r", encoding='UTF-8') as file:
+            building_data = json.load(file)
+
+        if not building_data["features"]:
+            continue
+
+        colors = {}
+        polygons = []
+        keys = []
+
+        for feature in building_data["features"]:
+            geometry = feature["geometry"].get("coordinates")
+            key = feature["properties"].get("key")
+            polygon = Polygon(geometry[0])
+            polygons.append(polygon)
+            keys.append(key)
+
+        polygon_index_map = {dumps(p): i for i, p in enumerate(polygons)}
+
+        polygon_strings = set(polygon_index_map.keys())
+
+        polygons_unique = [loads(ps) for ps in polygon_strings]
+        indexes_unique = [polygon_index_map[ps] for ps in polygon_strings]
+
+        polygons = polygons_unique
+        combined_polygons = polygons_unique
+        combined_keys = []
+
+        for i in range(len(polygons)):
+            combined_keys.append(keys[indexes_unique[i]])
+
+        indexes_to_remove = []
+
+        for i in range(len(polygons)):
+            for j in range(i + 1, len(polygons)):
+                if polygons[i].contains(polygons[j]):
+                    indexes_to_remove.append(i)
+                    if combined_keys[j] == "residence":
+                        combined_keys[j] = combined_keys[i]
+                elif polygons[j].contains(polygons[i]):
+                    indexes_to_remove.append(j)
+                    if combined_keys[i] == "residence":
+                        combined_keys[i] = combined_keys[j]
+
+        indexes_to_remove = list(set(indexes_to_remove))
+        indexes_to_remove.sort(reverse=True)
+
+        for idx in indexes_to_remove:
+            del combined_keys[idx]
+            del combined_polygons[idx]
+
+        geojson_polygons = [geojson.Feature(geometry=polygon, properties={"key": key}) for polygon, key in zip(combined_polygons, combined_keys)]
+
+        feature_collection = geojson.FeatureCollection(geojson_polygons)
+
         building_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
-                                         'Buildings', f'{city_name}_buildings{i}.geojson')
+                                         'Combined_Buildings', f'{city_name}_buildings{index}.geojson')
 
+        with open(building_filename, 'w') as f:
+             json.dump(feature_collection, f)
 
-        if os.path.exists(building_filename):
-            with open(building_filename, "r", encoding='UTF-8') as file:
-                building_data = json.load(file)
+import shutil
 
-            colors = {}
-            polygons = []
-            keys = []
+dir = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Buildings')
+if os.path.exists(dir):
+    shutil.rmtree(dir)
 
-            for feature in building_data["features"]:
-                geometry = feature["geometry"].get("coordinates")
-                key = feature["properties"].get("key")
-                polygon = Polygon(geometry[0])
-                polygons.append(polygon)
-                keys.append(key)
+temp_dir = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Combined_Buildings')
 
-# 각 폴리곤 문자열에 대해 원래의 인덱스를 기록하는 사전을 생성합니다.
-polygon_index_map = {dumps(p): i for i, p in enumerate(polygons)}
-
-# 중복된 폴리곤을 제거합니다.
-polygon_strings = set(polygon_index_map.keys())
-
-# 중복을 제거한 폴리곤과 그에 대응하는 원래의 인덱스를 저장하는 리스트를 생성합니다.
-polygons_unique = [loads(ps) for ps in polygon_strings]
-indexes_unique = [polygon_index_map[ps] for ps in polygon_strings]
-
-print(indexes_unique)
-
-polygons = polygons_unique
-
-for i in range(len(polygons)):
-    for j in range(i + 1, len(polygons)):
-        if polygons[i].contains(polygons[j]):
-            print(f"Polygon {indexes_unique[i+1]} contains Polygon {indexes_unique[j+1]}")
-
-            # print(f"Polygon {i + 1} contains Polygon {j + 1}")
-
-        elif polygons[j].contains(polygons[i]):
-            print(f"Polygon {indexes_unique[j+1]} contains Polygon {indexes_unique[i + 1]}")
-
-            # print(f"Polygon {j + 1} contains Polygon {i + 1}")
+os.rename(temp_dir, dir)
