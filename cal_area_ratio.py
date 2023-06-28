269e3bc63645953a53fef4af13b91028c0175f20
mi-song <misong023@khu.ac.kr>
2023-06-28 16:56:47 +0900

    [FIX] image - building 외곽선 추가

--- cal_area_ratio.py
+++ cal_area_ratio.py
@@ -5,11 +5,19 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import sys
 
+from shapely.geometry import Polygon, shape
+
 current_script_path = os.path.dirname(os.path.abspath(__file__))
 husg_directory_path = os.path.dirname(current_script_path)
 sys.path.append(husg_directory_path)
 
 from etc import variables as variables
+
+
+index_li = []
+building_list = []
+boundary_list = []
+
 
 # 자를 영역 추출
 def get_square_bounds(geojson_path):
@@ -44,7 +52,7 @@ def get_square_bounds(geojson_path):
     return left, upper, right, lower
 
 
-def image(city_name):
+def calarearatio(city_name):
     dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Boundaries')
     files = os.listdir(dir_path)
     filenum = len(files)
@@ -90,12 +98,12 @@ def image(city_name):
                     print(f"{building_filename} resulted in an empty GeoDataFrame after applying filter, skipping...")
                     continue
 
-                fig, ax = plt.subplots(figsize=(2.24, 2.24), dpi=100)
+                # fig, ax = plt.subplots(figsize=(2.24, 2.24), dpi=100)
 
                 with open(boundary_filename, "r", encoding='UTF-8') as file:
                     boundary_data = json.load(file)
                 boundary_gdf = gpd.GeoDataFrame.from_features(boundary_data, crs="EPSG:4326")
-                boundary_gdf.plot(ax=ax, color='white', edgecolor='black', linewidth=0.3)
+                # boundary_gdf.plot(ax=ax, color='white', edgecolor='black', linewidth=0.3)
 
                 gdf_cut = gdf_cut.dropna(subset=['color'])
                 # Check if GeoDataFrame is empty after dropna
@@ -103,12 +111,59 @@ def image(city_name):
                     print(f"{building_filename} resulted in an empty GeoDataFrame after dropna, skipping...")
                     continue
 
-                gdf_cut.plot(color=gdf_cut['color'], alpha=0.5, ax=ax, edgecolor='white', linewidth=0.1)
+                # gdf_cut.plot(color=gdf_cut['color'], alpha=0.5, ax=ax)
 
-                ax.set_axis_off()
+                # ax.set_axis_off()
+
+                building_polygons = []
+
+                for feature in boundary_data['features']:
+                    boundary_geometry = shape(feature['geometry'])
+
+                boundary_area = boundary_geometry.area
+
+                for feature in building_data['features']:
+                    geometry = shape(feature['geometry'])
+
+                    if geometry.geom_type == 'Polygon':
+                        building_polygons.append(geometry)
+
+                    elif geometry.geom_type == 'MultiPolygon':
+                        for polygon in geometry.geoms:
+                            building_polygons.append(polygon)
+
+                building_area = 0
+
+                for polygon in range(len(building_polygons)):
+                    building_area += building_polygons[polygon].area
+
+                building_list.append(building_area)
+                boundary_list.append(boundary_area)
 
                 image_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team',
                                               f'{city_name}_dataset', 'Image',
                                               f'{city_name}_buildings_image{index}.png')
 
-                plt.savefig(image_filename, dpi=100, transparent=True)
+                index_li.append(index)
+
+                # plt.savefig(image_filename, dpi=100, transparent=True)
+
+
+def getarearatio(city_name):
+    calarearatio(city_name='firenze')
+    filenames = []
+    for i in range(len(index_li)):
+        if (building_list[i]/boundary_list[i])*100 < 10:
+            filenames.append(index_li[i])
+
+    return filenames
+
+calarearatio(city_name='minneapolis')
+
+count = 0
+
+for i in range(3337):
+    if (building_list[i]/boundary_list[i])*100 < 10:
+        count += 1
+    print(f"area ratio: {(building_list[i]/boundary_list[i])*100}, filename: {index_li[i]}")
+    # print(count)
