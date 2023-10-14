59bc4d056106d21b089923e95712cbba021902d0
SeongraeNoh <rhosunr99@khu.ac.kr>
2023-10-15 01:27:25 +0900

    [ADD] normalize.py: process list of cities, add tqdm

--- get_data/normalize.py
+++ get_data/normalize.py
@@ -2,9 +2,11 @@ import os
 import numpy as np
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 from shapely.geometry import Polygon, LineString, Point
 
 from etc.cityname import *
+
 city_name = city_name
 
 dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset', 'Boundaries')
@@ -67,56 +69,86 @@ def normalize_coordinates(geometry, min_x, min_y, max_x, max_y):
 
 fig, ax = plt.subplots()
 
-for i in range(1, 100):
-    building_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
-                                     'filtered_data','Buildings', f'{city_name}_buildings{i}.geojson')
-    boundary_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
-                                     'filtered_data','Boundaries', f'{city_name}_boundaries{i}.geojson')
 
-    if os.path.exists(building_filename):
-        boundary_gdf = gpd.read_file(boundary_filename)
-        building_gdf = gpd.read_file(building_filename)
+def sort_key(filename):
+    # 파일 이름에서 숫자만 추출
+    num = int(''.join(filter(str.isdigit, filename)))
+    return num
 
-        # Find bounding box
-        min_x = min(building_gdf.geometry.bounds.minx.min(), boundary_gdf.geometry.bounds.minx.min())
-        min_y = min(building_gdf.geometry.bounds.miny.min(), boundary_gdf.geometry.bounds.miny.min())
-        max_x = max(building_gdf.geometry.bounds.maxx.max(), boundary_gdf.geometry.bounds.maxx.max())
-        max_y = max(building_gdf.geometry.bounds.maxy.max(), boundary_gdf.geometry.bounds.maxy.max())
 
-        # Normalize coordinates
-        building_gdf['geometry'] = building_gdf['geometry'].apply(normalize_coordinates,
-                                                                  args=(min_x, min_y, max_x, max_y))
-        boundary_gdf['geometry'] = boundary_gdf['geometry'].apply(normalize_coordinates,
-                                                                  args=(min_x, min_y, max_x, max_y))
+for city_name in city_names:
+    building_dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
+                                     'filtered_data', 'Buildings')
+    boundary_dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
+                                     'filtered_data', 'Boundaries')
 
-        # Save the normalized data
-        # new_dir_path = "path_to_new_directory"
-        building_new_dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
-                                             'Normalized', 'Buildings')
-        boundary_new_dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
-                                             'Normalized', 'Boundaries')
-        os.makedirs(building_new_dir_path, exist_ok=True)
-        os.makedirs(boundary_new_dir_path, exist_ok=True)
+    # Iterate over all .geojson files in the directory
+    for building_filepath in tqdm(sorted([f for f in os.listdir(building_dir_path) if f.endswith('.geojson')], key=sort_key)):
+        num = sort_key(building_filepath)
+        boundary_filepath = building_filepath.replace('buildings', 'boundaries')
 
-        building_gdf.to_file(os.path.join(building_new_dir_path, f'{city_name}_buildings{i}.geojson'), driver='GeoJSON')
-        boundary_gdf.to_file(os.path.join(boundary_new_dir_path, f'{city_name}_boundaries{i}.geojson'), driver='GeoJSON')
+        # Construct the full paths
+        building_filename = os.path.join(building_dir_path, building_filepath)
+        boundary_filename = os.path.join(boundary_dir_path, boundary_filepath)
 
-        # # Get building polygons for the current file and add them to the building_polygon list
-        # building_polygon = [row['geometry'] for idx, row in building_gdf.iterrows()]
-        #
-        # # Accumulate building polygons from all files into the building_polygons list
-        #
-        # """
-        # building_polygons : list of building polygons in boundary  ex. [POLYGON ((x1 y1, x2 y2, ...)), POLYGON ((x1 y1, x2 y2, ...))]
-        # boundary_polygon : boundary polygon  ex. POLYGON ((x1 y1, x2 y2, ...))
-        #
-        # x1, y1은 현재 위도 경도
-        # """
-        #
-        # building_polygons.extend(building_polygon)
-        # boundary_polygon = boundary_gdf.iloc[0]['geometry']
-        #
-        # # 시각화
-        # plot_boundary_building(building_polygons, boundary_polygon)
-        #
-        # building_polygons.clear()
+        if os.path.exists(building_filename):
+
+            boundary_gdf = gpd.read_file(boundary_filename)
+            building_gdf = gpd.read_file(building_filename)
+            # for i in range(1, 10000):
+            #     building_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
+            #                                      'filtered_data','Buildings', f'{city_name}_buildings{i}.geojson')
+            #     boundary_filename = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team', f'{city_name}_dataset',
+            #                                      'filtered_data','Boundaries', f'{city_name}_boundaries{i}.geojson')
+            #
+            #     if os.path.exists(building_filename):
+            #         boundary_gdf = gpd.read_file(boundary_filename)
+            #         building_gdf = gpd.read_file(building_filename)
+
+            # Find bounding box
+            min_x = min(building_gdf.geometry.bounds.minx.min(), boundary_gdf.geometry.bounds.minx.min())
+            min_y = min(building_gdf.geometry.bounds.miny.min(), boundary_gdf.geometry.bounds.miny.min())
+            max_x = max(building_gdf.geometry.bounds.maxx.max(), boundary_gdf.geometry.bounds.maxx.max())
+            max_y = max(building_gdf.geometry.bounds.maxy.max(), boundary_gdf.geometry.bounds.maxy.max())
+
+            # Normalize coordinates
+            building_gdf['geometry'] = building_gdf['geometry'].apply(normalize_coordinates,
+                                                                      args=(min_x, min_y, max_x, max_y))
+            boundary_gdf['geometry'] = boundary_gdf['geometry'].apply(normalize_coordinates,
+                                                                      args=(min_x, min_y, max_x, max_y))
+
+            # Save the normalized data
+            # new_dir_path = "path_to_new_directory"
+            building_new_dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team',
+                                                 f'{city_name}_dataset',
+                                                 'Normalized', 'Buildings')
+            boundary_new_dir_path = os.path.join('Z:', 'iiixr-drive', 'Projects', '2023_City_Team',
+                                                 f'{city_name}_dataset',
+                                                 'Normalized', 'Boundaries')
+            os.makedirs(building_new_dir_path, exist_ok=True)
+            os.makedirs(boundary_new_dir_path, exist_ok=True)
+
+            building_gdf.to_file(os.path.join(building_new_dir_path, f'{city_name}_buildings{num}.geojson'),
+                                 driver='GeoJSON')
+            boundary_gdf.to_file(os.path.join(boundary_new_dir_path, f'{city_name}_boundaries{num}.geojson'),
+                                 driver='GeoJSON')
+
+            # # Get building polygons for the current file and add them to the building_polygon list
+            # building_polygon = [row['geometry'] for idx, row in building_gdf.iterrows()]
+            #
+            # # Accumulate building polygons from all files into the building_polygons list
+            #
+            # """
+            # building_polygons : list of building polygons in boundary  ex. [POLYGON ((x1 y1, x2 y2, ...)), POLYGON ((x1 y1, x2 y2, ...))]
+            # boundary_polygon : boundary polygon  ex. POLYGON ((x1 y1, x2 y2, ...))
+            #
+            # x1, y1은 현재 위도 경도
+            # """
+            #
+            # building_polygons.extend(building_polygon)
+            # boundary_polygon = boundary_gdf.iloc[0]['geometry']
+            #
+            # # 시각화
+            # plot_boundary_building(building_polygons, boundary_polygon)
+            #
+            # building_polygons.clear()
