b84c8a3b53ce15c2f18f044fd6d327dab3ecc484
SeungWon Seo <ttd8591@gmail.com>
2023-10-19 01:54:07 +0900

    [FIX] fix dataloader dataset dir path

--- network/transformer/dataloader.py
+++ network/transformer/dataloader.py
@@ -9,14 +9,37 @@ class BoundaryDataset(Dataset):
     """
 
     def __init__(self, train_ratio=0.8, val_ratio=0.1, test_ratio=0.1, data_type='train'):
-        self.dataset_path = './dataset/husg_transformer_dataset.npz'
-        self.sub_dataset_path = './dataset/husg_unit_coords.pkl'
+        self.dataset_path = '../../../../mnt/2_transformer/train_dataset/'
+        self.city_names = ["atlanta", "dallas", "houston", "lasvegas", "littlerock",
+                           "philadelphia", "phoenix", "portland", "richmond", "saintpaul",
+                           "sanfrancisco", "miami", "seattle", "boston", "providence",
+                           "neworleans", "denver", "pittsburgh", "tampa", "washington"]
+        self.file_name = '/husg_transformer_dataset.npz'
 
-        self.dataset = np.load(self.dataset_path)
-        with open(self.sub_dataset_path, 'rb') as f:
-            self.unit_coords_data = pickle.load(f)
+        # Initialize empty lists for each dataset key
+        all_unit_position_datasets = []
+        all_street_unit_position_datasets = []
+        all_building_index_sequences = []
+        all_street_index_sequences = []
+        all_unit_coords_datasets = []
 
-        self.total_size = len(self.dataset['unit_position_datasets'])
+        for city_name in self.city_names:
+            data = np.load(self.dataset_path + city_name + self.file_name)
+
+            all_unit_position_datasets.append(data['unit_position_datasets'])
+            all_street_unit_position_datasets.append(data['street_unit_position_datasets'])
+            all_building_index_sequences.append(data['building_index_sequences'])
+            all_street_index_sequences.append(data['street_index_sequences'])
+            all_unit_coords_datasets.append(data['unit_coords_data'])
+
+        # Concatenate data from all cities for each key
+        self.unit_position_datasets = np.concatenate(all_unit_position_datasets, axis=0)
+        self.street_unit_position_datasets = np.concatenate(all_street_unit_position_datasets, axis=0)
+        self.building_index_sequences = np.concatenate(all_building_index_sequences, axis=0)
+        self.street_index_sequences = np.concatenate(all_street_index_sequences, axis=0)
+        self.unit_coords_datasets = np.concatenate(all_unit_coords_datasets, axis=0)
+
+        self.total_size = len(self.unit_position_datasets)
         if data_type == 'train':
             self.start_index = 0
             self.end_index = int(self.total_size * train_ratio)
@@ -27,11 +50,11 @@ class BoundaryDataset(Dataset):
             self.start_index = int(self.total_size * (train_ratio + val_ratio))
             self.end_index = int(self.total_size * (train_ratio + val_ratio + test_ratio))
 
-        self.unit_position_datasets = self.dataset['unit_position_datasets'][self.start_index:self.end_index]
-        self.street_unit_position_datasets = self.dataset['street_unit_position_datasets'][self.start_index:self.end_index]
-        self.building_index_sequences = self.dataset['building_index_sequences'][self.start_index:self.end_index]
-        self.street_index_sequences = self.dataset['street_index_sequences'][self.start_index:self.end_index]
-        self.unit_coords_data = self.unit_coords_data[self.start_index:self.end_index]
+        self.unit_position_datasets = self.unit_position_datasets[self.start_index:self.end_index]
+        self.street_unit_position_datasets = self.street_unit_position_datasets[self.start_index:self.end_index]
+        self.building_index_sequences = self.building_index_sequences[self.start_index:self.end_index]
+        self.street_index_sequences = self.street_index_sequences[self.start_index:self.end_index]
+        self.unit_coords_datasets = self.unit_coords_datasets[self.start_index:self.end_index]
 
     def __getitem__(self, index):
         """
@@ -46,9 +69,11 @@ class BoundaryDataset(Dataset):
         unit_position_dataset = self.unit_position_datasets[index]
         street_position_dataset = self.street_unit_position_datasets[index]
         building_index_sequence = self.building_index_sequences[index]
-        street_index_sequences = self.street_index_sequences[index]
+        street_index_sequence = self.street_index_sequences[index]
+        unit_coords_dataset = self.unit_coords_datasets[index]
 
-        return unit_position_dataset, street_position_dataset, building_index_sequence, street_index_sequences
+        return unit_position_dataset, street_position_dataset, building_index_sequence, street_index_sequence, \
+               unit_coords_dataset
 
     def __len__(self):
         """
