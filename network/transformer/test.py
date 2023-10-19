a9cc9abda90af207142e40a0f27a7b1de9f6e8d1
SeungWon Seo <ttd8591@gmail.com>
2023-10-19 11:35:47 +0900

    temp

--- network/transformer/test.py
+++ network/transformer/test.py
@@ -107,8 +107,10 @@ class Trainer:
                 trg_seq = torch.full_like(trg_building_seq, fill_value=self.eos_idx).to(device=self.device)
                 trg_seq[:, 0] = 0
                 for t in range(0, self.n_boundary - 1):
+                    print(trg_seq)
                     output = self.transformer(src_unit_seq, src_street_seq, trg_seq, trg_street_seq)
                     next_token = (torch.sigmoid(output) > 0.5).long()[:, t]
+                    print(next_token)
                     trg_seq[:, t + 1] = next_token
 
                 # Compute the losses
