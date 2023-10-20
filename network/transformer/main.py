91c07c502bb0e8b3f5760e14c3181ea09ce9de96
SeongraeNoh <rhosunr99@khu.ac.kr>
2023-10-20 23:51:14 +0900

    temp

--- network/transformer/main.py
+++ network/transformer/main.py
@@ -112,7 +112,7 @@ class Trainer:
         Returns:
         - torch.Tensor: Computed BCE loss.
         """
-        loss = F.binary_cross_entropy(torch.sigmoid(pred[:, :-1]), trg[:, 1:], reduction='none')
+        loss = F.binary_cross_entropy(torch.sigmoid(pred[:, 1:]), trg[:, 1:], reduction='none')
 
         # pad_idx에 해당하는 레이블을 무시하기 위한 mask 생성
         mask = get_pad_mask(trg[:, 1:], pad_idx=self.pad_idx).float()
