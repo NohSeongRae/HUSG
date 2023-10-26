768efb8d1fad15acb168066646eba37444e0b416
SeungWon Seo <ttd8591@gmail.com>
2023-10-26 14:44:11 +0900

    temp

--- network/transformer_graph/dataloader.py
+++ network/transformer_graph/dataloader.py
@@ -83,7 +83,7 @@ class GraphDataset(Dataset):
                         elif dataset_name == 'adj_matrices':
                             zeros = np.zeros((n_street + n_building, n_street + n_building))
                             zeros[0] = 1
-                            zeros[1:len(data) + 1, :len(data)] = data
+                            zeros[1:len(data) + 1, 1:len(data)+1] = data
                             zeros[len(data) + 1] = 1
                             zeros[len(data) + 2:] = 0
                             data = zeros
--- network/transformer_graph/model.py
+++ network/transformer_graph/model.py
@@ -36,9 +36,6 @@ def get_trg_street_mask(adj_matrix, n_street_node):
     # Clip values greater than 1
     street_mask = torch.clamp(street_mask, max=1)
 
-    for m in street_mask[0][:50]:
-        print(m[:50])
-
     return street_mask.bool()
 
 def get_trg_local_mask(adj_matrix):
