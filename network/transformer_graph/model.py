af3ea1f55e65d9c866c98960cde3d94947be08ef
SeungWon Seo <ttd8591@gmail.com>
2023-10-26 16:44:32 +0900

    temp

--- network/transformer_graph/model.py
+++ network/transformer_graph/model.py
@@ -162,6 +162,8 @@ class GraphTransformer(nn.Module):
         trg_street_mask = get_trg_street_mask(trg_adj_seq, n_street_node) & trg_global_mask
         trg_local_mask = get_trg_local_mask(trg_adj_seq) & trg_global_mask
 
+        print(trg_sub_mask.shape, trg_global_mask.shape, trg_street_mask.shape, trg_local_mask.shape)
+
         dec_output = self.decoder(trg_adj_seq, enc_output, trg_global_mask, trg_street_mask, trg_local_mask, src_global_mask)
 
         output = self.dropout(dec_output)
