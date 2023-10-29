7eb407a530ba5de397fac781e4281660e158307e
SeungWon Seo <ttd8591@gmail.com>
2023-10-29 17:07:57 +0900

    [FIX] fix test code

--- network/transformer_graph/test.py
+++ network/transformer_graph/test.py
@@ -45,7 +45,9 @@ def cross_entropy_loss(pred, trg, pad_idx):
     Returns:
     - torch.Tensor: Computed BCE loss.
     """
-    loss = F.binary_cross_entropy(torch.sigmoid(pred[:, :-1]), get_clipped_adj_matrix(trg[:, 1:]), reduction='none')
+    weights = (trg[:, 1:].clone() * 1) + 1
+    loss = F.binary_cross_entropy(torch.sigmoid(pred[:, :-1]), get_clipped_adj_matrix(trg[:, 1:]), reduction='none',
+                                  weight=weights)
 
     # pad_idx에 해당하는 레이블을 무시하기 위한 mask 생성
     pad_mask = get_pad_mask(trg[:, 1:, 0], pad_idx=pad_idx)
@@ -55,27 +57,26 @@ def cross_entropy_loss(pred, trg, pad_idx):
     # mask 적용
     masked_loss = loss * mask.float()
     # 손실의 평균 반환
-    return masked_loss.mean()
+    return masked_loss.sum() / mask.float().sum()
 
-def test(sos_idx, eos_idx, pad_idx, d_street, d_unit, d_model, n_layer, n_head,
+def test(sos_idx, eos_idx, pad_idx, n_street, d_street, d_unit, d_model, n_layer, n_head,
          n_building, n_boundary, dropout, checkpoint_epoch, train_ratio, val_ratio, test_ratio,
          use_global_attn, use_street_attn, use_local_attn, save_dir_path):
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
     # Subsequent initializations will use the already loaded full dataset
-    test_dataset = GraphDataset(train_ratio=train_ratio, val_ratio=val_ratio, test_ratio=test_ratio,
-                                data_type='test', load=False)
+    test_dataset = GraphDataset(data_type='test', n_street=n_street, n_building=n_building, n_boundary=n_boundary, d_unit=d_unit, d_street=d_street)
     test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=8)
 
     # Initialize the Transformer model
     transformer = GraphTransformer(n_building=n_building, sos_idx=sos_idx, eos_idx=eos_idx,
-                                        pad_idx=pad_idx,
-                                        d_street=d_street, d_unit=d_unit, d_model=d_model,
-                                        d_inner=d_model * 4, n_layer=n_layer, n_head=n_head,
-                                        dropout=dropout,
-                                        use_global_attn=use_global_attn,
-                                        use_street_attn=use_street_attn,
-                                        use_local_attn=use_local_attn).to(device=device)
+                                    pad_idx=pad_idx,
+                                    d_street=d_street, d_unit=d_unit, d_model=d_model,
+                                    d_inner=d_model * 4, n_layer=n_layer, n_head=n_head,
+                                    dropout=dropout, n_street=n_street,
+                                    use_global_attn=use_global_attn,
+                                    use_street_attn=use_street_attn,
+                                    use_local_attn=use_local_attn).to(device=device)
 
     checkpoint = torch.load("./models/" + save_dir_path + "/transformer_epoch_"+ str(checkpoint_epoch) + ".pth")
     transformer.load_state_dict(checkpoint['model_state_dict'])
@@ -129,12 +130,10 @@ if __name__ == '__main__':
     parser.add_argument("--n_head", type=int, default=8, help="Number of attention heads.")
     parser.add_argument("--n_building", type=int, default=120, help="binary classification for building existence.")
     parser.add_argument("--n_boundary", type=int, default=250, help="Number of boundary or token.")
+    parser.add_argument("--n_street", type=int, default=60, help="Number of boundary or token.")
     parser.add_argument("--dropout", type=float, default=0.1, help="Dropout rate used in the transformer model.")
     parser.add_argument("--seed", type=int, default=327, help="Random seed for reproducibility across runs.")
     parser.add_argument("--checkpoint_epoch", type=int, default=0, help="Use checkpoint index.")
-    parser.add_argument("--train_ratio", type=float, default=0.89, help="Use checkpoint index.")
-    parser.add_argument("--val_ratio", type=float, default=0.01, help="Use checkpoint index.")
-    parser.add_argument("--test_ratio", type=float, default=0.1, help="Use checkpoint index.")
     parser.add_argument("--use_global_attn", type=bool, default=True, help="Use checkpoint index.")
     parser.add_argument("--use_street_attn", type=bool, default=True, help="Use checkpoint index.")
     parser.add_argument("--use_local_attn", type=bool, default=True, help="Use checkpoint index.")
@@ -152,9 +151,8 @@ if __name__ == '__main__':
     torch.manual_seed(opt.seed)
     torch.cuda.manual_seed_all(opt.seed)
 
-    test(sos_idx=opt.sos_idx, eos_idx=opt.eos_idx, pad_idx=opt.pad_idx,
+    test(sos_idx=opt.sos_idx, eos_idx=opt.eos_idx, pad_idx=opt.pad_idx, n_street=opt.n_street,
          d_street=opt.d_street, d_unit=opt.d_unit, d_model=opt.d_model, n_layer=opt.n_layer, n_head=opt.n_head,
          n_building=opt.n_building, n_boundary=opt.n_boundary, dropout=opt.dropout, checkpoint_epoch=opt.checkpoint_epoch,
-         train_ratio=opt.train_ratio, val_ratio=opt.val_ratio, test_ratio=opt.test_ratio,
          use_global_attn=opt.use_global_attn, use_street_attn=opt.use_street_attn, use_local_attn=opt.use_local_attn,
          save_dir_path=opt.save_dir_path)
