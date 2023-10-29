b86088605c826dafa16a0cf96e80cdab222069df
SeungWon Seo <ttd8591@gmail.com>
2023-10-29 17:53:14 +0900

    temp

--- network/transformer_graph/visualization.py
+++ network/transformer_graph/visualization.py
@@ -8,8 +8,10 @@ def get_all_one_rows(matrix):
 
 def plot(pred, gt, idx, cur_n_street):
     one_indices = get_all_one_rows(gt)
-    for p in pred:
-        print(p)
+    for idx in range(len(pred)):
+        print(idx)
+        print(pred[idx])
+        print(gt[idx])
     i1 = one_indices[0] + 1
     i2 = one_indices[1]
 
