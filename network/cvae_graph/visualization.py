3a361378aeaa85bb51c81934731431de2bde3c44
SeungWon Seo <ttd8591@gmail.com>
2023-11-02 18:43:58 +0900

    [FIX] fix visualization

--- network/cvae_graph/visualization.py
+++ network/cvae_graph/visualization.py
@@ -75,7 +75,7 @@ def plot(pos, size, rot, mask, gt, idx):
         rotated_points = rotate_points_around_center(points, [x, y], theta)
 
         rotated_points = np.array(rotated_points)
-        rotated_box = np.concatenate((rotated_points, [rotated_points[0]]), axis=0)[0]
+        rotated_box = np.concatenate((rotated_points, [rotated_points[0]]), axis=0)
         ax2.plot(rotated_box[:, 0], rotated_box[:, 1], 'r-', label='Rotated Box')
 
     # Set the aspect of the plot to be equal
