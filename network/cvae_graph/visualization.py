6383b96c4aa534958c3d3826ac27032e3ca9f1cb
SeungWon Seo <ttd8591@gmail.com>
2023-11-03 00:17:20 +0900

    test code

--- network/cvae_graph/visualization.py
+++ network/cvae_graph/visualization.py
@@ -22,8 +22,10 @@ def rotate_points_around_center(points, center, theta_deg):
     print(theta_deg)
     if theta_deg > 90:
         theta_deg = 180 - theta_deg
-    else:
+    elif theta_deg > 45:
         theta_deg = theta_deg
+    else:
+        theta_deg += 90
 
     # Convert theta from degrees to radians
     theta_rad = np.radians(theta_deg)
