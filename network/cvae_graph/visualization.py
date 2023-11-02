3ccf20efc4069101a42e613f34fdbf504980d735
SeungWon Seo <ttd8591@gmail.com>
2023-11-02 19:23:41 +0900

    [FIX] fix visualization

--- network/cvae_graph/visualization.py
+++ network/cvae_graph/visualization.py
@@ -19,13 +19,13 @@ def get_bbox_corners(x, y, w, h):
 
 
 def rotate_points_around_center(points, center, theta_deg):
-    if theta_deg > 90:
-        theta_deg = 90 - theta_deg
-    else:
+    if theta_deg < 45:
         theta_deg = theta_deg
+    else:
+        theta_deg = 90 - theta_deg
 
     # Convert theta from degrees to radians
-    theta_rad = np.radians(theta_deg)
+    theta_rad = np.radians(theta_deg * 2)
 
     # Create a rotation matrix
     rotation_matrix = np.array([
