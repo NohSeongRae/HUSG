ef22265a96e97e21711ead13a072400f430377e7
SeungWon Seo <ttd8591@gmail.com>
2023-11-03 13:49:58 +0900

    temp

--- network/cvae_graph/preprocessing.py
+++ network/cvae_graph/preprocessing.py
@@ -65,6 +65,7 @@ def preprocesing_dataset(train_ratio=0.8, val_ratio=0.1, test_ratio=0.1,
             zeros[:len(building_feature)] = building_feature
 
             if np.any((zeros < 0) | (zeros > 1)):
+                print(zeros)
                 continue
             for i in range(len(node_features[idx])):
                 a.append(node_features[idx][i, 1])
