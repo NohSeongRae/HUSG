866cbdeac05dfd381dcba438e86534314d12cede
SeungWon Seo <ttd8591@gmail.com>
2023-11-04 16:08:06 +0900

    [ADD] add mask condition for visualization

--- network/cvae_graph/visualization.py
+++ network/cvae_graph/visualization.py
@@ -48,7 +48,7 @@ def rotate_points_around_center(points, center, theta_deg):
 
     return rotated_points
 
-def plot(pos, size, rot, building_exist_mask, condition, gt, idx):
+def plot(pos, size, rot, building_exist_mask, gt, condition, idx):
     # Create a figure and axes
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
     rotation_scale = 180
