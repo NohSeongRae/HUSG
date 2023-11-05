95cebb519701853e85e770058ee9047cebe703b5
SeungWon Seo <ttd8591@gmail.com>
2023-11-05 21:17:09 +0900

    [FIX] fix test code

--- network/cvae_graph/test.py
+++ network/cvae_graph/test.py
@@ -73,13 +73,23 @@ def test(d_feature, d_latent, n_head, T, checkpoint_epoch, save_dir_path, only_b
             # print(f"Epoch {idx + 1}/{len(test_dataloader)} - Validation Loss Theta: {loss_theta:.4f}")
             # print(f"Epoch {idx + 1}/{len(test_dataloader)} - Validation Loss KL: {loss_kl:.4f}")
 
-            plot(output_pos.detach().cpu().numpy(),
-                 output_size.detach().cpu().numpy(),
-                 output_theta.detach().cpu().numpy(),
-                 data.building_mask.detach().cpu().numpy(),
-                 data.building_feature.detach().cpu().numpy(),
-                 data.condition.detach().cpu().numpy(),
-                 idx + 1)
+            if only_building_graph:
+                plot(output_pos.detach().cpu().numpy(),
+                     output_size.detach().cpu().numpy(),
+                     output_theta.detach().cpu().numpy(),
+                     None,
+                     data.building_feature.detach().cpu().numpy(),
+                     data.condition.detach().cpu().numpy(),
+                     idx + 1)
+
+            else:
+                plot(output_pos.detach().cpu().numpy(),
+                     output_size.detach().cpu().numpy(),
+                     output_theta.detach().cpu().numpy(),
+                     data.building_mask.detach().cpu().numpy(),
+                     data.building_feature.detach().cpu().numpy(),
+                     data.condition.detach().cpu().numpy(),
+                     idx + 1)
 
 
 if __name__ == '__main__':
