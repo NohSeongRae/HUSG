433cb0fde418f9608899fbe672e8a48ccafb5c7b
SeungWon Seo <ttd8591@gmail.com>
2023-11-06 10:42:09 +0900

    [FIX] fix shuffle

--- network/cvae_graph/main.py
+++ network/cvae_graph/main.py
@@ -71,14 +71,14 @@ class Trainer:
         self.train_dataset = GraphDataset(data_type='train',
                                           condition_type=condition_type, chunk_graph=chunk_graph)
         self.train_sampler = DistributedSampler(dataset=self.train_dataset, rank=rank)
-        self.train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=False,
+        self.train_dataloader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True,
                                            sampler=self.train_sampler, num_workers=8, pin_memory=True)
 
         # Subsequent initializations will use the already loaded full dataset
         self.val_dataset = GraphDataset(data_type='val',
                                         condition_type=condition_type, chunk_graph=chunk_graph)
         self.val_sampler = DistributedSampler(dataset=self.val_dataset, rank=rank)
-        self.val_dataloader = DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False,
+        self.val_dataloader = DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=True,
                                          sampler=self.val_sampler, num_workers=8, pin_memory=True)
 
         # Initialize the Transformer model
