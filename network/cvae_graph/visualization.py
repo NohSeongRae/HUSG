2a42cc47c1ef4011244af246a768f13ef411a09a
SeungWon Seo <ttd8591@gmail.com>
2023-11-11 07:04:27 +0900

    [FIX] fix building semantics

--- network/cvae_graph/visualization.py
+++ network/cvae_graph/visualization.py
@@ -9,6 +9,7 @@ import seaborn as sns
 
 def get_random_color(seed, palette='pastel', n_colors=15):
     seed = int(seed)
+    print(seed)
     random.seed(seed)
     colors = sns.color_palette(palette, n_colors)
     return colors[random.randint(0, len(colors) - 1)]
