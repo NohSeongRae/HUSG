d317ad53a4d7bac4c6534388dd9e556945b6a102
SeungWon Seo <ttd8591@gmail.com>
2023-11-16 23:27:07 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -139,6 +139,10 @@ class GraphDataset(Dataset):
 
             edge_index = nx.to_scipy_sparse_matrix(graph).tocoo()
             edge_index = torch.tensor(np.vstack((edge_index.row, edge_index.col)), dtype=torch.long)
+
+            mask = ((building_masks[edge_index[0]] == 1) & (building_masks[edge_index[1]] == 1)).squeeze(-1)
+            edge_index = edge_index[:, mask]
+
             # PyG 데이터 객체를 생성합니다.
             data = Data(node_features=node_features, node_semantics=node_semantics,
                         building_mask=building_masks, condition=condition,
