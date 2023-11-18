6b73cb1072f2ae9ab5cd10e8a7e05e03f6e19845
SeungWon Seo <ttd8591@gmail.com>
2023-11-18 11:11:32 +0900

    only building

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -133,7 +133,12 @@ class GraphDataset(Dataset):
             edge_index = torch.tensor(np.vstack((edge_index.row, edge_index.col)), dtype=torch.long)
 
             mask = ((building_masks[edge_index[0]] == 1) & (building_masks[edge_index[1]] == 1)).squeeze(-1)
-            edge_index = edge_index[:, mask]
+            building_edge_index = edge_index[:, mask]
+
+            mask = ((building_masks[edge_index[0]] == 0) & (building_masks[edge_index[1]] == 0)).squeeze(-1)
+            street_edge_index = edge_index[:, mask]
+
+            edge_index = torch.cat([building_edge_index, street_edge_index], dim=1)
 
             # PyG 데이터 객체를 생성합니다.
             data = Data(node_features=node_features, node_semantics=node_semantics,
