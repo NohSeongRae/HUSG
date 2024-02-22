1f738e61acf700a54920d6da4815c96026f367b8
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-02-22 13:29:29 +0900

    [Add] add test code

---
+++ globalmapper/test.py
@@ -0,0 +1,85 @@
+import argparse
+import torch
+import torch.nn.functional as F
+from torch_geometric.loader import DataLoader
+
+import numpy as np
+import random
+from tqdm import tqdm
+
+from model import GraphCVAE
+from dataloader import GraphDataset
+from visualization import plot
+
+def test(d_feature, d_latent, n_head, T, checkpoint_epoch, save_dir_path, condition_type, convlayer):
+    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+
+    test_dataset = GraphDataset(data_type='test', condition_type=condition_type)
+    test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=8)
+
+    cvae = GraphCVAE(T=T, feature_dim=d_feature, latent_dim=d_latent, n_head=n_head,
+                     condition_type=condition_type, convlayer=convlayer).to(device=device)
+
+    if checkpoint_epoch == 0:
+        checkpoint_epoch = 'best'
+    checkpoint = torch.load("./models/" + save_dir_path + "/epoch_" + str(checkpoint_epoch) + ".pth")
+    cvae.load_state_dict(checkpoint['model_state_dict'])
+
+    cvae.eval()
+    with torch.no_grad():
+        for idx, data in enumerate(tqdm(test_dataloader)):
+            data, polygon_path, data_path  = data
+
+            data = data.to(device=device)
+            output_pos, output_size, output_theta = cvae.test(data)
+
+            if condition_type == 'image' or condition_type == 'image_resnet34':
+                plot(output_pos.detach().cpu().numpy(),
+                     output_size.detach().cpu().numpy(),
+                     output_theta.detach().cpu().numpy(),
+                     data.building_mask.detach().cpu().numpy(),
+                     data.node_features.detach().cpu().numpy(),
+                     idx + 1,
+                     condition_type,
+                     polygon_path,
+                     save_dir_path,
+                     data_path)
+
+            elif condition_type == 'graph':
+                plot(output_pos.detach().cpu().numpy(),
+                     output_size.detach().cpu().numpy(),
+                     output_theta.detach().cpu().numpy(),
+                     data.building_mask.detach().cpu().numpy(),
+                     data.node_features.detach().cpu().numpy(),
+                     idx + 1,
+                     condition_type,
+                     polygon_path,
+                     save_dir_path,
+                     data_path)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Initialize a transformer with user-defined hyperparameters.")
+
+    parser.add_argument("--T", type=int, default=3, help="Dimension of the model.")
+    parser.add_argument("--d_feature", type=int, default=256, help="Dimension of the model.")
+    parser.add_argument("--d_latent", type=int, default=512, help="Dimension of the model.")
+    parser.add_argument("--n_head", type=int, default=8, help="Dimension of the model.")
+    parser.add_argument("--seed", type=int, default=327, help="Random seed for reproducibility across runs.")
+    parser.add_argument("--checkpoint_epoch", type=int, default=0, help="Use checkpoint index.")
+    parser.add_argument("--save_dir_path", type=str, default="cvae_graph", help="save dir path")
+    parser.add_argument("--condition_type", type=str, default='image_resnet34', help="save dir path")
+    parser.add_argument("--convlayer", type=str, default='gat', help="save dir path")
+
+    opt = parser.parse_args()
+
+    for arg in vars(opt):
+        print(f"{arg}: {getattr(opt, arg)}")
+
+    random.seed(opt.seed)
+    np.random.seed(opt.seed)
+    torch.manual_seed(opt.seed)
+    torch.cuda.manual_seed_all(opt.seed)
+
+    test(d_feature=opt.d_feature, d_latent=opt.d_latent, n_head=opt.n_head, T=opt.T,
+         checkpoint_epoch=opt.checkpoint_epoch, save_dir_path=opt.save_dir_path,
+         condition_type=opt.condition_type, convlayer=opt.convlayer)
---
+++ globalmapper/visualization.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pickle
+
+def get_random_color(seed):
+    palette = plt.cm.tab10
+    norm_index = seed / 10
+    color = palette(norm_index)
+    return color
+
+def get_bbox_corners(x, y, w, h):
+    half_w = w / 2
+    half_h = h / 2
+
+    top_left = [x - half_w, y - half_h]
+    top_right = [x + half_w, y - half_h]
+    bottom_left = [x - half_w, y + half_h]
+    bottom_right = [x + half_w, y + half_h]
+
+    return [top_left, top_right, bottom_right, bottom_left]
+
+def rotate_points_around_center(points, center, theta_deg):
+    theta_rad = np.radians(theta_deg)
+
+    rotation_matrix = np.array([
+        [np.cos(theta_rad), -np.sin(theta_rad)],
+        [np.sin(theta_rad), np.cos(theta_rad)]
+    ])
+
+    points = np.array(points)
+    center = np.array(center)
+    translated_points = points - center
+
+    rotated_points = np.dot(translated_points, rotation_matrix.T)
+    rotated_points = rotated_points + center
+
+    return rotated_points
+
+def plot(pos, size, rot, building_exist_mask, gt_features, idx, condition_type, polygon_path=None, save_dir_path='', data_path=None):
+    directory = f"./images_{condition_type}/{save_dir_path}/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    save_path_2 = os.path.join(directory, "file_name_" + str(idx) + ".png")
+    with open(save_path_2.replace('.png', '.pkl'), 'wb') as file:
+        file_name = [data_path[0]]
+        print(file_name)
+
+        if 'stockholm' in file_name[0]:
+            print('It is stockholm')
+            return
+
+        pickle.dump(file_name, file)
+
+    fig, ax1 = plt.subplots(1, 1, figsize=(6, 6))
+    fig, ax2 = plt.subplots(1, 1, figsize=(6, 6))
+    rotation_scale = 45
+
+    pred_output_list = []
+    for i in range(len(pos)):
+        if building_exist_mask[i] == 0:
+            continue
+
+        x, y, w, h, theta = pos[i][0], pos[i][1], size[i][0], size[i][1], (rot[i][0] * 2 - 1) * rotation_scale
+        pred_output_list.append([x, y, w, h, theta])
+
+        points = get_bbox_corners(x, y, w, h)
+        rotated_points = rotate_points_around_center(points, [x, y], theta)
+
+        rotated_points = np.array(rotated_points)
+        rotated_box = np.concatenate((rotated_points, [rotated_points[0]]), axis=0)
+
+        ax1.plot(rotated_box[:, 0], rotated_box[:, 1], color='k', label='Rotated Box')
+
+    gt_output_list = []
+    for i in range(len(pos)):
+        if building_exist_mask[i] == 0:
+            continue
+        x, y, w, h, theta = gt_features[i][0], gt_features[i][1], gt_features[i][2], gt_features[i][3], (gt_features[i][4] * 2 - 1) * rotation_scale,
+        gt_output_list.append([x, y, w, h, theta])
+
+    if polygon_path == None:
+        filepath = f'../../../..//local_datasets/{condition_type}_condition_train_datasets/' + 'test/' + str(idx - 1) + '.pkl'
+        with open(filepath, 'rb') as f:
+            building_polygons = pickle.load(f)
+    else:
+        filepath = f'../../../..//local_datasets/{condition_type}_condition_train_datasets/' + 'test/' + polygon_path[0]
+        with open(filepath, 'rb') as f:
+            building_polygons = pickle.load(f)
+
+    if data_path != None:
+        filepath = f'../../../..//local_datasets/{condition_type}_condition_train_datasets/' + 'test/' + data_path[0]
+        with open(filepath, 'rb') as f:
+            gpickle_file = pickle.load(f)
+
+    for building_polygon in building_polygons:
+        x, y = building_polygon
+        ax2.plot(x, y, color='k', label='Rotated Box')
+
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+
+    ax1.set_aspect('equal', adjustable='box')
+    ax1.set_xlim([0.0, 1.0])
+    ax1.set_ylim([0.0, 1.0])
+    ax1.set_axis_off()
+    save_path_1 = os.path.join(directory, "prediction_" + str(idx) + ".png")
+    ax1.figure.savefig(save_path_1, dpi=300, bbox_inches='tight')
+
+    ax2.set_aspect('equal', adjustable='box')
+    ax2.set_xlim([0.0, 1.0])
+    ax2.set_ylim([0.0, 1.0])
+    ax2.set_axis_off()
+    save_path_2 = os.path.join(directory, "ground_truth_" + str(idx) + ".png")
+    ax2.figure.savefig(save_path_2, dpi=300, bbox_inches='tight')
+
+    with open(save_path_1.replace('.png', '.pkl'), 'wb') as file:
+        pickle.dump(pred_output_list, file)
+
+    with open(save_path_2.replace('.png', '.pkl'), 'wb') as file:
+        pickle.dump(gt_output_list, file)
+
+    with open(save_path_2.replace('.png', '.gpickle'), 'wb') as file:
+        pickle.dump(gpickle_file, file)
+
+    save_path_2 = os.path.join(directory, "real_polygon_" + str(idx) + ".png")
+    with open(save_path_2.replace('.png', '.pkl'), 'wb') as file:
+        pickle.dump(building_polygons, file)
+
+    print(save_path_1)
