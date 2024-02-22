122efeb60b1460cdbe4626b8f1c7678b3c2fa93f
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-02-22 20:28:54 +0900

    temp

--- globalmapper/dataloader.py
+++ globalmapper/dataloader.py
@@ -68,7 +68,8 @@ class GraphDataset(Dataset):
             size_features = torch.cat((size_x_features, size_y_features), dim=1)
 
             if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
-                condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
+                condition = torch.tensor(np.array(graph.graph['binary_mask']), dtype=torch.float32).unsqueeze(0)
+                condition = torch.cat([condition, condition, condition], dim=0)
             else:
                 condition_graph = graph.graph['condition']
                 condition_edge_index = nx.to_scipy_sparse_matrix(condition_graph).tocoo()
