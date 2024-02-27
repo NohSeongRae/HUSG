b4231a3012734f624e5949af51a8dc696fbba7f9
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-02-28 06:42:29 +0900

    temp

--- preprocessing/global_mapper/inverse_cst_test.py
+++ preprocessing/global_mapper/inverse_cst_test.py
@@ -13,6 +13,7 @@ from shapely.affinity import scale
 from shapely.ops import transform
 
 test_gt_graph = "./datasets/test/20.gpickle"
+test_gt_graph = "./output/1.gpickle"
 
 def move_polygon_center_to_midpoint(polygon):
     minx, miny, maxx, maxy = polygon.bounds
