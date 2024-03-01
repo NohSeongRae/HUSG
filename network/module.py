20ba6dfec0794523acbd13e3ed9820af72cedde2
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-03-01 21:37:47 +0900

    temp

--- network/module.py
+++ network/module.py
@@ -13,6 +13,7 @@ class ScaledDotProductAttention(nn.Module):
     def forward(self, q, k, v, mask=None):
         attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
 
+        print(attn.shape, mask.shape)
         if mask is not None:
             attn = attn.masked_fill(mask == 0, -1e9)
 
