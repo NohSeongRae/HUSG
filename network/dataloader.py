2ddccb86c7193ec5a566eae8d0cb4911fbe9f04a
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-03-01 21:31:19 +0900

    temp

--- network/dataloader.py
+++ network/dataloader.py
@@ -37,8 +37,8 @@ class GraphDataset(Dataset):
         padded_matrix = np.pad(matrix, pad_width=pad_width, mode='constant', constant_values=0)
 
         # 패딩 마스크 생성: 실제 데이터는 0, 패딩 부분은 1
-        pad_mask = np.zeros_like(padded_matrix, dtype=np.float32)
-        pad_mask[:original_shape[0], :original_shape[1]] = 1  # 실제 데이터 부분을 1로 설정
+        pad_mask = np.zeros_like(len(padded_matrix), dtype=np.float32)
+        pad_mask[:original_shape[0]] = 1  # 실제 데이터 부분을 1로 설정
 
         return padded_matrix, pad_mask
 
