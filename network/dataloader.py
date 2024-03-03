b3d01e8b38db08a4e222860602f1ebabd016230e
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-03-03 21:18:37 +0900

    temp

--- network/dataloader.py
+++ network/dataloader.py
@@ -61,6 +61,7 @@ class GraphDataset(Dataset):
         building_adj_matrix_padded, building_pad_mask = self.pad_matrix(building_adj_matrix, (120, 120))
         bb_adj_matrix_padded, bb_pad_mask = self.pad_matrix(bb_adj_matrix, (120, 200))
         boundary_pos_padded, _ = self.pad_matrix(boundary_pos_feature, (120, 2))
+        print(boundary_pos_padded.shape)
 
         return {
             'boundary_adj_matrix_padded': torch.tensor(boundary_adj_matrix_padded, dtype=torch.float32),
