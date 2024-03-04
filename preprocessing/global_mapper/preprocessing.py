314065e244cc974fb32ab55b48d61df84532a34c
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-03-04 15:28:43 +0900

    temp

--- preprocessing/global_mapper/preprocessing.py
+++ preprocessing/global_mapper/preprocessing.py
@@ -15,31 +15,32 @@ def generate_datasets(idx, data_type):
     n_building = len(buildings)
     n_chunk = n_node - n_building
 
-    adj_matrix = nx.adjacency_matrix(graph).todense()
-    boundary_adj_matrix = adj_matrix[:n_chunk, :n_chunk]
-    building_adj_matrix = adj_matrix[n_chunk:, n_chunk:]
-    bb_adj_matrix = adj_matrix[n_chunk:, :n_chunk]
-    boundary_pos_feature = []
+    if n_building >= 2:
+        adj_matrix = nx.adjacency_matrix(graph).todense()
+        boundary_adj_matrix = adj_matrix[:n_chunk, :n_chunk]
+        building_adj_matrix = adj_matrix[n_chunk:, n_chunk:]
+        bb_adj_matrix = adj_matrix[n_chunk:, :n_chunk]
+        boundary_pos_feature = []
 
-    for node in graph.nodes():
-        if node < n_chunk:
-            boundary_pos_feature.append(graph.nodes[node]['node_features'][:2])
-    boundary_pos_feature = np.array(boundary_pos_feature)
+        for node in graph.nodes():
+            if node < n_chunk:
+                boundary_pos_feature.append(graph.nodes[node]['node_features'][:2])
+        boundary_pos_feature = np.array(boundary_pos_feature)
 
-    data = {'boundary_adj_matrix': boundary_adj_matrix,
-            'building_adj_matrix': building_adj_matrix,
-            'bb_adj_matrix': bb_adj_matrix,
-            'boundary_pos_feature': boundary_pos_feature,
-            'n_boundary': n_chunk,
-            'n_building': n_building}
+        data = {'boundary_adj_matrix': boundary_adj_matrix,
+                'building_adj_matrix': building_adj_matrix,
+                'bb_adj_matrix': bb_adj_matrix,
+                'boundary_pos_feature': boundary_pos_feature,
+                'n_boundary': n_chunk,
+                'n_building': n_building}
 
-    output_file_path = f'graph_generation_datasets/{data_type}/'
-    with open(f'{output_file_path}/{idx}.pickle', 'wb') as f:
-        pickle.dump(data, f)
+        output_file_path = f'graph_generation_datasets/{data_type}/'
+        with open(f'{output_file_path}/{idx}.pickle', 'wb') as f:
+            pickle.dump(data, f)
 
 if __name__ == '__main__':
     end_index = 208622 + 1
-    data_type = 'test'
+    data_type = 'train'
 
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = []
