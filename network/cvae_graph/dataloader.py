10e956b41f1cce09c987be8fac23b6088ea2a6de
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-03-05 04:54:13 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -68,7 +68,8 @@ class GraphDataset(Dataset):
 
             if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
                 condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
-                print(condition.shape)
+                condition = condition.unsqueeze(0)
+                condition = condition.repeat(3, 1, 1)
             else:
                 condition_graph = graph.graph['condition']
                 condition_edge_index = nx.to_scipy_sparse_matrix(condition_graph).tocoo()
