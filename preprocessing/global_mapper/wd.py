8725629493b2bfc56e2d5386f22752e2647187d6
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-03-07 02:48:13 +0900

    temp

--- preprocessing/global_mapper/wd.py
+++ preprocessing/global_mapper/wd.py
@@ -7,7 +7,7 @@ import pickle
 
 
 base = 'grid_graph_figure'
-base = 'output/synthetic_T4_dnpcs_ariel-k1/cvae_graph_20240305_145257'
+base = 'output/without_image_condition_spatial_graph_ariel-k1/cvae_graph_20240306_115843'
 path = ''
 path = os.path.join(base, path)
 
