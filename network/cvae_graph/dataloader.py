aece2389bc4813e1dcfbf244d6a19e4a3d42861e
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-25 18:34:59 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -25,9 +25,9 @@ class GraphDataset(Dataset):
         if condition_type == 'graph':
             self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
         elif condition_type == 'image':
-            self.folder_path = '/local_datasets/image_condition_train_datasets/' + self.data_type
+            self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
         elif condition_type == 'image_resnet34':
-            self.folder_path = '/local_datasets/gt_train_datasets/' + self.data_type
+            self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
         file_extension = '.gpickle'
 
         count = 0
