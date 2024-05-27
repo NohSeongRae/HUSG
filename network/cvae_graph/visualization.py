cd5f5e12d1fc2163c254d9669ce0ebb036913b75
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-27 12:04:48 +0900

    [Add] add align function

--- network/cvae_graph/visualization.py
+++ network/cvae_graph/visualization.py
@@ -37,14 +37,53 @@ def rotate_points_around_center(points, center, theta_deg):
 
     return rotated_points
 
+def calculate_angle(p1, p2):
+    """
+    Calculate the angle of the line segment connecting points p1 and p2.
+    """
+    return np.arctan2(p2[1] - p1[1], p1[0] - p2[0])
+
+def find_closest_boundary_segment(building_center, boundary_coords):
+    """
+    Find the closest boundary segment to the given building center
+    and return the angle of that segment and the points of the closest segment.
+    """
+    min_distance = float('inf')
+    closest_angle = None
+    closest_segment = None
+
+    for i in range(len(boundary_coords) - 1):
+        p1 = boundary_coords[i]
+        p2 = boundary_coords[i + 1]
+        segment_angle = calculate_angle(p1, p2)
+
+        # Calculate the midpoint of the boundary segment
+        segment_midpoint = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
+
+        # Calculate the distance between the building center and the segment midpoint
+        dist = np.linalg.norm(np.array(building_center) - np.array(segment_midpoint))
+
+        if dist < min_distance:
+            min_distance = dist
+            closest_angle = segment_angle
+            closest_segment = (p1, p2)
+
+    return closest_angle, closest_segment
+
 def plot(pos, size, rot, building_exist_mask, gt_features, idx, condition_type, polygon_path=None, save_dir_path='', data_path=None):
     directory = f"./synthetic_images_{condition_type}/{save_dir_path}/"
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    boundary_coords = []
+    for i in range(len(pos)):
+        if building_exist_mask[i] == 1:
+            continue
+        x, y = gt_features[i][0], gt_features[i][1]
+        boundary_coords.append([x, y])
+
     fig, ax1 = plt.subplots(1, 1, figsize=(6, 6))
     fig, ax2 = plt.subplots(1, 1, figsize=(6, 6))
-    rotation_scale = 45
 
     pred_output_list = []
     for i in range(len(pos)):
@@ -52,10 +91,13 @@ def plot(pos, size, rot, building_exist_mask, gt_features, idx, condition_type, 
             continue
 
         x, y, w, h = pos[i][0], pos[i][1], size[i][0], size[i][1]
-        pred_output_list.append([x, y, w, h, 0])
+        closest_angle, closest_segment = find_closest_boundary_segment([x, y], boundary_coords)
+        angle = np.degrees(closest_angle)
+
+        pred_output_list.append([x, y, w, h, angle])
 
         points = get_bbox_corners(x, y, w, h)
-        rotated_points = rotate_points_around_center(points, [x, y], 0)
+        rotated_points = rotate_points_around_center(points, [x, y], angle)
 
         rotated_points = np.array(rotated_points)
         rotated_box = np.concatenate((rotated_points, [rotated_points[0]]), axis=0)
@@ -66,11 +108,11 @@ def plot(pos, size, rot, building_exist_mask, gt_features, idx, condition_type, 
     for i in range(len(pos)):
         if building_exist_mask[i] == 0:
             continue
-        x, y, w, h = gt_features[i][0], gt_features[i][1], gt_features[i][2], gt_features[i][3]
-        gt_output_list.append([x, y, w, h, 0])
+        x, y, w, h, theta = gt_features[i][0], gt_features[i][1], gt_features[i][2], gt_features[i][3], (gt_features[i][4] * 2 - 1) * 45,
+        gt_output_list.append([x, y, w, h, theta])
 
         points = get_bbox_corners(x, y, w, h)
-        rotated_points = rotate_points_around_center(points, [x, y], 0)
+        rotated_points = rotate_points_around_center(points, [x, y], theta)
 
         rotated_points = np.array(rotated_points)
         rotated_box = np.concatenate((rotated_points, [rotated_points[0]]), axis=0)
