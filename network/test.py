5614ecc76b48579cd0df9955262aa0b1727c2562
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-27 10:36:43 +0900

    ttemp

--- network/test.py
+++ network/test.py
@@ -67,7 +67,22 @@ if __name__ == '__main__':
             output = transformer(building_adj_matrix_padded, boundary_adj_matrix_padded,
                                  building_pad_mask, boundary_pad_mask, boundary_pos_padded)
             output *= bb_pad_mask
-            output = (output >= 0.5).float()
+            output_1 = (output >= 0.5).float()
+
+            k = 3  # 상위 k개 값을 선택
+
+            # 각 120x200 행렬에 대해 top k 값을 찾기
+            topk_values, topk_indices = torch.topk(output, k, dim=2)
+
+            # 모든 값을 0으로 설정한 다음, topk 인덱스에 해당하는 위치만 1로 설정
+            output_2 = torch.zeros_like(output)
+            output_2.scatter_(2, topk_indices, 1)
+
+            output = torch.zeros_like(output_1)
+            if torch.sum(output_1) <= n_building * k:
+                output = output_2
+            else:
+                output = output_1
 
             pred_adj_matrix = np.zeros((n_boundary + n_building, n_boundary + n_building))
             gt_adj_matrix = np.zeros((n_boundary + n_building, n_boundary + n_building))
