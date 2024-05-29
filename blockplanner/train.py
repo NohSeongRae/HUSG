291f0b53a21c72c5e5496b85202f9e50e05c2353
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-29 21:08:15 +0900

    temp

--- blockplanner/train.py
+++ blockplanner/train.py
@@ -336,7 +336,7 @@ if __name__ == '__main__':
     parser.add_argument("--kl_weight", type=float, default=1, help="save dir path")
     parser.add_argument("--shape_weight", type=float, default=0.05, help="save dir path")
     parser.add_argument("--condition_type", type=str, default='image', help="save dir path")
-    parser.add_argument("--convlayer", type=str, default='gat', help="save dir path")
+    parser.add_argument("--convlayer", type=str, default='gcn', help="save dir path")
 
     opt = parser.parse_args()
 
