9e604f5d7ba5b3810002c0113c0e9e26c50dcd4e
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-30 16:14:44 +0900

    temp

--- layout_vae/test.py
+++ layout_vae/test.py
@@ -164,8 +164,9 @@ def evaluate_and_visualize(model, loader, loss, save_dir, prefix='', colors=None
                 c[has_box, :] = state[1][-1]
 
         for i in range(batch_size):
-            save_path_1 = os.path.join(save_dir, f"{filename}_ground_truth.png")    # 실제 결과 저장 경로
-            save_path_2 = os.path.join(save_dir, f"{filename}_prediction.png")      # 예측 결과 저장 경로
+            number = filename.replace("(\'", "").replace(".gpickle\',)", "")
+            save_path_1 = os.path.join(save_dir, f"ground_truth_{number}.png")    # 실제 결과 저장 경로
+            save_path_2 = os.path.join(save_dir, f"prediction_{number}.png")      # 예측 결과 저장 경로
 
             plot_layout(
                 boxes[i].detach().cpu().numpy(),
