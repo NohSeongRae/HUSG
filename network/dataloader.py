7b44010e539d6b7231df197a6924a434c435538b
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-31 01:13:28 +0900

    temp

--- network/dataloader.py
+++ network/dataloader.py
@@ -17,9 +17,9 @@ class GraphDataset(Dataset):
 
         self.data_type = data_type
 
-        self.folder_path = f'/local_datasets/urban_datasets/datasets/graph_generation_datasets/{data_type}'
+        self.folder_path = f'/local_datasets/urban_datasets/datasets/eu_graph_generation_datasets/{data_type}'
         if data_type == 'test':
-            self.folder_path = f'/local_datasets/urban_datasets/datasets/graph_generation_datasets/{data_type}'
+            self.folder_path = f'/local_datasets/urban_datasets/datasets/eu_graph_generation_datasets/{data_type}'
 
         file_extension = '.pickle'
         count = 0
