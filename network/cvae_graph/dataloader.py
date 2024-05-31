3d71d4b7ea4543e614e1e2c5814528f4304f88a2
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-31 11:01:07 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -30,7 +30,7 @@ class GraphDataset(Dataset):
             self.folder_path = '/local_datasets/urban_datasets/datasets/eu_ours_graph_datasets/' + self.data_type
 
         if self.data_type == 'test':
-            self.folder_path = './synthetic_datasets_d_model_1024/'
+            self.folder_path = './synthetic_datasets_eu/'
 
         file_extension = '.gpickle'
 
