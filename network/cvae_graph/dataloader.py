729a5f14622d5ecd42a421f467f074d02555cd91
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-05-31 14:25:54 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -23,11 +23,11 @@ class GraphDataset(Dataset):
         self.data_type = data_type
 
         if condition_type == 'graph':
-            self.folder_path = '/local_datasets/urban_datasets/datasets/eu_ours_graph_datasets/' + self.data_type
+            self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
         elif condition_type == 'image':
-            self.folder_path = '/local_datasets/urban_datasets/datasets/eu_ours_graph_datasets/' + self.data_type
+            self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
         elif condition_type == 'image_resnet34':
-            self.folder_path = '/local_datasets/urban_datasets/datasets/eu_ours_graph_datasets/' + self.data_type
+            self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
 
         # if self.data_type == 'test':
         #     self.folder_path = './synthetic_datasets_eu/'
@@ -61,16 +61,34 @@ class GraphDataset(Dataset):
         - Data object or tuple: Depending on the data type, returns either a Data object for the graph data or a tuple containing the Data object, the path to polygon data, and the filename for test data.
         """
 
+        def filter_nodes(graph):
+            filtered_nodes = [node for node in graph.nodes() if graph.nodes[node]['building_masks'] == 1]
+            return filtered_nodes
+
+        def get_filtered_tensors(graph, filtered_nodes, node_mapping):
+            node_features = torch.tensor(np.array([graph.nodes[node]['node_features'] for node in filtered_nodes]),
+                                         dtype=torch.float32)
+            building_masks = torch.tensor(np.array([graph.nodes[node]['building_masks'] for node in filtered_nodes]),
+                                          dtype=torch.long)
+            return node_features, building_masks
+
+        def update_edges(graph, filtered_nodes):
+            subgraph = graph.subgraph(filtered_nodes)
+            node_mapping = {node: i for i, node in enumerate(filtered_nodes)}
+            updated_edges = [(node_mapping[u], node_mapping[v]) for u, v in subgraph.edges()]
+            edge_index = torch.tensor(updated_edges, dtype=torch.long).t().contiguous()
+            return edge_index
+
         if self.data_type == 'train' or self.data_type == 'val':
             load_path = self.folder_path + '/' + self.gpickle_files[idx]
             with open(load_path, 'rb') as f:
                 self.graph = pickle.load(f)
 
             graph = self.graph
-            node_features = torch.tensor(np.array([graph.nodes[node]['node_features'] for node in graph.nodes()]),
-                                         dtype=torch.float32)
-            building_masks = torch.tensor(np.array([graph.nodes[node]['building_masks'] for node in graph.nodes()]),
-                                          dtype=torch.long)
+            filtered_nodes = filter_nodes(graph)
+            node_mapping = {node: i for i, node in enumerate(filtered_nodes)}
+            node_features, building_masks = get_filtered_tensors(graph, filtered_nodes, node_mapping)
+            edge_index = update_edges(graph, filtered_nodes)
 
             if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
                 condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
@@ -89,12 +107,9 @@ class GraphDataset(Dataset):
                                  edge_index=condition_edge_index,
                                  num_nodes=condition_graph.number_of_nodes())
 
-            edge_index = nx.to_scipy_sparse_matrix(graph).tocoo()
-            edge_index = torch.tensor(np.vstack((edge_index.row, edge_index.col)), dtype=torch.long)
-
             data = Data(node_features=node_features,
                         building_mask=building_masks, condition=condition,
-                        edge_index=edge_index, num_nodes=graph.number_of_nodes())
+                        edge_index=edge_index, num_nodes=len(filtered_nodes))
 
             return data
         else:
@@ -103,10 +118,10 @@ class GraphDataset(Dataset):
                 self.graph = pickle.load(f)
             graph = self.graph
 
-            node_features = torch.tensor(np.array([graph.nodes[node]['node_features'] for node in graph.nodes()]),
-                                         dtype=torch.float32)
-            building_masks = torch.tensor(np.array([graph.nodes[node]['building_masks'] for node in graph.nodes()]),
-                                          dtype=torch.long)
+            filtered_nodes = filter_nodes(graph)
+            node_mapping = {node: i for i, node in enumerate(filtered_nodes)}
+            node_features, building_masks = get_filtered_tensors(graph, filtered_nodes, node_mapping)
+            edge_index = update_edges(graph, filtered_nodes)
 
             if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
                 condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
@@ -125,12 +140,9 @@ class GraphDataset(Dataset):
                                  edge_index=condition_edge_index,
                                  num_nodes=condition_graph.number_of_nodes())
 
-            edge_index = nx.to_scipy_sparse_matrix(graph).tocoo()
-            edge_index = torch.tensor(np.vstack((edge_index.row, edge_index.col)), dtype=torch.long)
-
             data = Data(node_features=node_features,
                         building_mask=building_masks, condition=condition,
-                        edge_index=edge_index, num_nodes=graph.number_of_nodes())
+                        edge_index=edge_index, num_nodes=len(filtered_nodes))
 
             polygon_path = self.gpickle_files[idx].replace('.gpickle', '.pkl')
             return (data, polygon_path, self.gpickle_files[idx])
