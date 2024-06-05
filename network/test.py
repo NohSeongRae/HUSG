e3a4df405c8f87520ac83622815520c6ef63bc22
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-06-05 19:49:35 +0900

    temp

--- network/test.py
+++ network/test.py
@@ -102,8 +102,8 @@ if __name__ == '__main__':
             gt_adj_matrix[n_boundary:, n_boundary:] = building_adj_matrix_padded.squeeze(0).detach().cpu().numpy()[:n_building, :n_building]
             gt_adj_matrix[n_boundary:, :n_boundary] = bb_adj_matrix_padded.squeeze(0).detach().cpu().numpy()[:n_building, :n_boundary]
 
-            path = f'/local_datasets/urban_datasets/datasets/eu_ours_graph_datasets/val/{file}.gpickle'
-            path = f'cvae_graph/large_scale_datasets/image_condition_train_datasets/{file}.gpickle'
+            path = f'/local_datasets/urban_datasets/datasets/ours_graph_datasets/test/{file}.gpickle'
+            # path = f'cvae_graph/large_scale_datasets/image_condition_train_datasets/{file}.gpickle'
             graph = nx.read_gpickle(path)
 
             graph.remove_edges_from(list(graph.edges()))
