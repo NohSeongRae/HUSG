45de4c00586aa72792addf373ddbe4c42326d0a6
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-17 15:21:51 +0900

    fix to gnn decoder

--- globalmapper/dataloader.py
+++ globalmapper/dataloader.py
@@ -23,7 +23,7 @@ class GraphDataset(Dataset):
         #         self.folder_path = f'../preprocessing/global_mapper/eu_gt_graph_datasets/{data_type}'
         # elif condition_type == 'image_resnet34':
         #     self.folder_path = '/local_datasets/globalmapper_datasets/'
-        self.folder_path = f'/local_datasets/urban_datasets/eu_global_mapper_datasets/{data_type}'
+        self.folder_path = f'/local_datasets/urban_datasets/global_mapper_datasets/{data_type}'
         file_extension = '.gpickle'
 
         count = 0
