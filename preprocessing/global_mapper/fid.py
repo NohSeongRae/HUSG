68ef10a9cdd1cc71fe231f943a20254a42b0c8d5
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-18 11:19:48 +0900

    fix to gnn decoder

--- preprocessing/global_mapper/fid.py
+++ preprocessing/global_mapper/fid.py
@@ -11,7 +11,7 @@ from torchmetrics.image.fid import FrechetInceptionDistance
 # path = os.path.join(base, path)
 # path = 'output/without_image_condition_spatial_graph_ariel-k1/cvae_graph_20240306_115843'
 path = "C:/Users/Dobby/Downloads/gt_images_image_resnet34/cvae_graph_20240529_135822_GIN"
-path = "C:/Users/Dobby/Downloads/synthetic_images_without_image_condition/cvae_graph_20240531_135245"
+path = "C:/Users/Dobby/Downloads/usa_gnn_decoder_output/cvae_graph_20240717_145920"
 list_output_all = os.listdir(path)
 
 list_output = []
