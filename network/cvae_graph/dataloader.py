fa6308d9df8d5b961523bad1e895f83d45078a09
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-18 11:21:26 +0900

    fix to gnn decoder

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -30,9 +30,10 @@ class GraphDataset(Dataset):
             self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
 
         if self.data_type == 'test':
-            self.folder_path = './large_scale_datasets/image_condition_train_datasets/'
+            # self.folder_path = './large_scale_datasets/image_condition_train_datasets/'
             # self.folder_path = './synthetic_datasets_large_scale/'
-            self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
+            self.folder_path = './synthetic_datasets/'
+            # self.folder_path = '/local_datasets/urban_datasets/datasets/ours_graph_datasets/' + self.data_type
             # self.folder_path = './random_graph_generation_datasets'
 
         file_extension = '.gpickle'
