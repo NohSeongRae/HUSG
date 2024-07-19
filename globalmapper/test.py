82cfdf2b2c9fbf371086566646222f9efa8fe05f
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-19 20:34:07 +0900

    temp

--- globalmapper/test.py
+++ globalmapper/test.py
@@ -49,9 +49,16 @@ def test(d_feature, d_latent, n_head, T, checkpoint_epoch, save_dir_path, condit
             data = data.to(device=device)
             output_pos, output_size, output_exist = cvae.test(data)
             output_exist = output_exist.detach().cpu().numpy()
+
+            exponential_dist = torch.distributions.Exponential(0.1)
+            samples = exponential_dist.sample((1,)).numpy()[0]
+            k = np.round(samples)
+
+            # output_exist 값을 기준으로 상위 k개의 노드를 선택
+            top_k_indices = torch.topk(output_exist, k=k, largest=True).indices
+
             for node in graph.nodes():
-                print(output_exist[node])
-                if output_exist[node] > 0.5:
+                if node in top_k_indices:
                     graph.nodes[node]['posx'] = output_pos[node][0].detach().cpu().numpy()
                     graph.nodes[node]['posy'] = output_pos[node][1].detach().cpu().numpy()
                     graph.nodes[node]['exist'] = 1.0
