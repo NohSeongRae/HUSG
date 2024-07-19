559f1cc93ffcac17afe8c18cfcc6d37f21a30bd7
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-19 16:53:55 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -96,10 +96,9 @@ class GraphDataset(Dataset):
                         pooled_node_features[i, 1] /= 2
                         pooled_node_features[i, 2] /= 2
                         pooled_node_features[i, 3] /= 2
-
+            print(pooled_node_features)
             pooled_building_masks = torch.zeros((pooled_boundary_n + building_n, 1), dtype=torch.long)
             pooled_building_masks[pooled_boundary_n:, 0] = 1
-            print(pooled_building_masks)
 
             if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
                 condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
