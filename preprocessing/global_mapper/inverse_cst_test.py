ada8b6515aeb0753a58109c93ed289577c91bb34
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-19 19:59:48 +0900

    temp

--- preprocessing/global_mapper/inverse_cst_test.py
+++ preprocessing/global_mapper/inverse_cst_test.py
@@ -55,7 +55,7 @@ def move_polygon_center_to_midpoint(polygon):
 
 count = 1
 for idx in tqdm(range(0, 21032)):
-    path = 'eu_global_mapper_output'
+    path = 'globalmapper_usa_rebuttal_output'
     test_gt_graph = f"./{path}/gt/{str(idx)}.gpickle"
     test_pred_graph = f"./{path}/pred/{str(idx)}.gpickle"
 
