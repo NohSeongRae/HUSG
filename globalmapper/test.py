b7c1ba085fe3a5b0d2f8b3508427012453c16edf
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-19 20:37:23 +0900

    temp

--- globalmapper/test.py
+++ globalmapper/test.py
@@ -56,7 +56,7 @@ def test(d_feature, d_latent, n_head, T, checkpoint_epoch, save_dir_path, condit
             print(k)
 
             # output_exist 값을 기준으로 상위 k개의 노드를 선택
-            top_k_indices = torch.topk(output_exist, k=k, largest=True).indices
+            top_k_indices = torch.topk(output_exist[0], k=k, largest=True).indices
 
             for node in graph.nodes():
                 if node in top_k_indices:
