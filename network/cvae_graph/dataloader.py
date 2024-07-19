b9a08c39bf77f300dc7400dfd05a9cd5c034245c
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-19 16:47:12 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -80,7 +80,6 @@ class GraphDataset(Dataset):
             building_n = torch.sum(building_masks).item()
             boundary_n = building_masks.shape[0] - building_n
             pooled_boundary_n = (boundary_n + 1) // 2
-            print(building_n, boundary_n, pooled_boundary_n)
 
             pooled_node_features = torch.zeros((pooled_boundary_n + building_n, 5))
             for i in range(pooled_boundary_n):
