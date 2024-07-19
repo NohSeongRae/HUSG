6ea1d615b448eb607e45e2a1b3a3b5a68c4dcceb
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-19 15:14:21 +0900

    fix random graph

--- network/dataloader.py
+++ network/dataloader.py
@@ -88,7 +88,7 @@ class GraphDataset(Dataset):
         n_building = self.data['n_building']
 
         pooled_n_boundary = (n_boundary + 1) // 2
-        pooled_boundary_adj_matrix = np.zeros(pooled_n_boundary, pooled_n_boundary)
+        pooled_boundary_adj_matrix = np.zeros((pooled_n_boundary, pooled_n_boundary))
         for i in range(pooled_n_boundary):
             for j in range(pooled_n_boundary):
                 for ii in range(2):
@@ -96,14 +96,14 @@ class GraphDataset(Dataset):
                         if i * 2 + ii < n_boundary and j * 2 + jj < n_boundary:
                             pooled_boundary_adj_matrix[i, j] += boundary_adj_matrix[i * 2 + ii, j * 2 + jj]
 
-        pooled_bb_adj_matrix = np.zeros(n_building, pooled_n_boundary)
+        pooled_bb_adj_matrix = np.zeros((n_building, pooled_n_boundary))
         for i in range(n_building):
             for j in range(pooled_n_boundary):
                 for ii in range(2):
                     if j * 2 + ii < n_boundary:
                         pooled_bb_adj_matrix[i, j] += bb_adj_matrix[i, j * 2 + ii]
 
-        pooled_boundary_pos_feature = np.zeros(pooled_n_boundary, 2)
+        pooled_boundary_pos_feature = np.zeros((pooled_n_boundary, 2))
         for i in range(pooled_n_boundary):
             for ii in range(2):
                 if i * 2 + ii < n_boundary:
