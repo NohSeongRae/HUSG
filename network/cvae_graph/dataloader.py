4b3b63539138e4bda35a779b366e2e8493e877dd
SeungWon Seo <62225673+ssw03270@users.noreply.github.com>
2024-07-20 11:19:40 +0900

    temp

--- network/cvae_graph/dataloader.py
+++ network/cvae_graph/dataloader.py
@@ -77,124 +77,34 @@ class GraphDataset(Dataset):
             building_masks = torch.tensor(np.array([graph.nodes[node]['building_masks'] for node in graph.nodes()]),
                                           dtype=torch.long)
 
-            building_n = torch.sum(building_masks).item()
-            boundary_n = building_masks.shape[0] - building_n
-            pooled_boundary_n = (boundary_n + 1) // 2
-
-            pooled_node_features = torch.zeros((pooled_boundary_n + building_n, 5))
-            for i in range(pooled_boundary_n):
-                for ii in range(2):
-                    if i * 2 + ii < boundary_n:
-                        pooled_node_features[i, 0] += node_features[i * 2 + ii, 0]
-                        pooled_node_features[i, 1] += node_features[i * 2 + ii, 1]
-                        pooled_node_features[i, 2] += node_features[i * 2 + ii, 2]
-                        pooled_node_features[i, 3] += node_features[i * 2 + ii, 3]
-                        if pooled_node_features[i, 4] == 0:
-                            pooled_node_features[i, 4] += node_features[i * 2 + ii, 4]
-                    if ii == 1:
-                        pooled_node_features[i, 0] /= 2
-                        pooled_node_features[i, 1] /= 2
-                        pooled_node_features[i, 2] /= 2
-                        pooled_node_features[i, 3] /= 2
-            for i in range(building_n):
-                pooled_node_features[i + pooled_boundary_n, 0] += node_features[i + boundary_n, 0]
-                pooled_node_features[i + pooled_boundary_n, 1] += node_features[i + boundary_n, 1]
-                pooled_node_features[i + pooled_boundary_n, 2] += node_features[i + boundary_n, 2]
-                pooled_node_features[i + pooled_boundary_n, 3] += node_features[i + boundary_n, 3]
-                pooled_node_features[i + pooled_boundary_n, 4] += node_features[i + boundary_n, 4]
-
-            pooled_building_masks = torch.zeros((pooled_boundary_n + building_n, 1), dtype=torch.long)
-            pooled_building_masks[pooled_boundary_n:, 0] = 1
-
-            if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
-                condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
-                condition = condition.unsqueeze(0)
-                condition = condition.repeat(3, 1, 1)
-            else:
-                condition_graph = graph.graph['condition']
-                condition_edge_index = nx.to_scipy_sparse_matrix(condition_graph).tocoo()
-                condition_edge_index = torch.tensor(np.vstack((condition_edge_index.row, condition_edge_index.col)),
-                                                    dtype=torch.long)
-                condition_street_feature = torch.tensor(
-                    np.array([condition_graph.nodes[node]['chunk_features'] for node in condition_graph.nodes()]),
-                    dtype=torch.float32)
-
-                condition = Data(condition_street_feature=condition_street_feature,
-                                 edge_index=condition_edge_index,
-                                 num_nodes=condition_graph.number_of_nodes())
-
-            # edge_index = nx.to_scipy_sparse_matrix(graph).tocoo()
-            # edge_index = torch.tensor(np.vstack((edge_index.row, edge_index.col)), dtype=torch.long)
-            adj_matrix = nx.to_numpy_array(graph)
-            boundary_adj_matrix = adj_matrix[:boundary_n, :boundary_n]
-            building_adj_matrix = adj_matrix[boundary_n:, boundary_n:]
-            bb_adj_matrix = adj_matrix[boundary_n:, :boundary_n]
-
-            pooled_n_boundary = (boundary_n + 1) // 2
-            pooled_boundary_adj_matrix = np.zeros((pooled_n_boundary, pooled_n_boundary))
-            for i in range(pooled_n_boundary):
-                for j in range(pooled_n_boundary):
-                    for ii in range(2):
-                        for jj in range(2):
-                            if i * 2 + ii < boundary_n and j * 2 + jj < boundary_n:
-                                pooled_boundary_adj_matrix[i, j] += boundary_adj_matrix[i * 2 + ii, j * 2 + jj]
-                                if pooled_boundary_adj_matrix[i, j] > 1:
-                                    pooled_boundary_adj_matrix[i, j] = 1
-
-            pooled_bb_adj_matrix = np.zeros((building_n, pooled_n_boundary))
-            for i in range(building_n):
-                for j in range(pooled_n_boundary):
-                    for ii in range(2):
-                        if j * 2 + ii < boundary_n:
-                            pooled_bb_adj_matrix[i, j] += bb_adj_matrix[i, j * 2 + ii]
-                            if pooled_bb_adj_matrix[i, j] > 1:
-                                pooled_bb_adj_matrix[i, j] = 1
-
-            pooled_adj_matrix = np.zeros((pooled_n_boundary + building_n, pooled_n_boundary + building_n))
-            pooled_adj_matrix[:pooled_n_boundary, :pooled_n_boundary] = pooled_boundary_adj_matrix
-            pooled_adj_matrix[pooled_n_boundary:, pooled_n_boundary:] = building_adj_matrix
-            pooled_adj_matrix[pooled_n_boundary:, :pooled_n_boundary] = pooled_bb_adj_matrix
-            pooled_adj_matrix[:pooled_n_boundary, pooled_n_boundary:] = pooled_bb_adj_matrix.T
-
-            import scipy.sparse as sp
-            sparse_matrix = sp.csr_matrix(pooled_adj_matrix)
-            coo_matrix = sparse_matrix.tocoo()
-            edge_index = torch.tensor(np.vstack((coo_matrix.row, coo_matrix.col)), dtype=torch.long)
-
             # building_n = torch.sum(building_masks).item()
             # boundary_n = building_masks.shape[0] - building_n
-            # unpooled_boundary_n = boundary_n * 2
+            # pooled_boundary_n = (boundary_n + 1) // 2
             #
-            # if unpooled_boundary_n > 200:
-            #     x = 200
-            # else:
-            #     x = unpooled_boundary_n
-            #
-            # unpooled_node_features = torch.zeros((x + building_n, 5))
-            # for i in range(x):
-            #     unpooled_node_features[i, 0] += node_features[i // 2, 0]
-            #     unpooled_node_features[i, 1] += node_features[i // 2, 1]
-            #     unpooled_node_features[i, 2] += node_features[i // 2, 2]
-            #     unpooled_node_features[i, 3] += node_features[i // 2, 3]
-            #     unpooled_node_features[i, 4] += node_features[i // 2, 4]
-            #
+            # pooled_node_features = torch.zeros((pooled_boundary_n + building_n, 5))
+            # for i in range(pooled_boundary_n):
+            #     for ii in range(2):
+            #         if i * 2 + ii < boundary_n:
+            #             pooled_node_features[i, 0] += node_features[i * 2 + ii, 0]
+            #             pooled_node_features[i, 1] += node_features[i * 2 + ii, 1]
+            #             pooled_node_features[i, 2] += node_features[i * 2 + ii, 2]
+            #             pooled_node_features[i, 3] += node_features[i * 2 + ii, 3]
+            #             if pooled_node_features[i, 4] == 0:
+            #                 pooled_node_features[i, 4] += node_features[i * 2 + ii, 4]
+            #         if ii == 1:
+            #             pooled_node_features[i, 0] /= 2
+            #             pooled_node_features[i, 1] /= 2
+            #             pooled_node_features[i, 2] /= 2
+            #             pooled_node_features[i, 3] /= 2
             # for i in range(building_n):
-            #     if unpooled_boundary_n > 200:
-            #         x = 200
-            #     else:
-            #         x = unpooled_boundary_n
-            #     unpooled_node_features[i + x, 0] += node_features[i, 0]
-            #     unpooled_node_features[i + x, 1] += node_features[i, 1]
-            #     unpooled_node_features[i + x, 2] += node_features[i, 2]
-            #     unpooled_node_features[i + x, 3] += node_features[i, 3]
-            #     unpooled_node_features[i + x, 4] += node_features[i, 4]
+            #     pooled_node_features[i + pooled_boundary_n, 0] += node_features[i + boundary_n, 0]
+            #     pooled_node_features[i + pooled_boundary_n, 1] += node_features[i + boundary_n, 1]
+            #     pooled_node_features[i + pooled_boundary_n, 2] += node_features[i + boundary_n, 2]
+            #     pooled_node_features[i + pooled_boundary_n, 3] += node_features[i + boundary_n, 3]
+            #     pooled_node_features[i + pooled_boundary_n, 4] += node_features[i + boundary_n, 4]
             #
-            # if unpooled_boundary_n > 200:
-            #     x = 200
-            # else:
-            #     x = unpooled_boundary_n
-            # unpooled_building_masks = torch.zeros((x + building_n, 1), dtype=torch.long)
-            # unpooled_building_masks[x:, 0] = 1
+            # pooled_building_masks = torch.zeros((pooled_boundary_n + building_n, 1), dtype=torch.long)
+            # pooled_building_masks[pooled_boundary_n:, 0] = 1
             #
             # if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
             #     condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
@@ -220,35 +130,125 @@ class GraphDataset(Dataset):
             # building_adj_matrix = adj_matrix[boundary_n:, boundary_n:]
             # bb_adj_matrix = adj_matrix[boundary_n:, :boundary_n]
             #
-            # unpooled_n_boundary = (boundary_n + 1) // 2
-            # unpooled_boundary_adj_matrix = np.zeros((x, x))
-            # for i in range(x):
-            #     for j in range(x):
-            #         unpooled_boundary_adj_matrix[i, j] = boundary_adj_matrix[i // 2, j // 2]
+            # pooled_n_boundary = (boundary_n + 1) // 2
+            # pooled_boundary_adj_matrix = np.zeros((pooled_n_boundary, pooled_n_boundary))
+            # for i in range(pooled_n_boundary):
+            #     for j in range(pooled_n_boundary):
+            #         for ii in range(2):
+            #             for jj in range(2):
+            #                 if i * 2 + ii < boundary_n and j * 2 + jj < boundary_n:
+            #                     pooled_boundary_adj_matrix[i, j] += boundary_adj_matrix[i * 2 + ii, j * 2 + jj]
+            #                     if pooled_boundary_adj_matrix[i, j] > 1:
+            #                         pooled_boundary_adj_matrix[i, j] = 1
             #
-            # unpooled_bb_adj_matrix = np.zeros((building_n, x))
+            # pooled_bb_adj_matrix = np.zeros((building_n, pooled_n_boundary))
             # for i in range(building_n):
-            #     for j in range(x):
-            #         unpooled_bb_adj_matrix[i, j] = bb_adj_matrix[i, j // 2]
+            #     for j in range(pooled_n_boundary):
+            #         for ii in range(2):
+            #             if j * 2 + ii < boundary_n:
+            #                 pooled_bb_adj_matrix[i, j] += bb_adj_matrix[i, j * 2 + ii]
+            #                 if pooled_bb_adj_matrix[i, j] > 1:
+            #                     pooled_bb_adj_matrix[i, j] = 1
             #
-            # if unpooled_n_boundary > 200:
-            #     unpooled_boundary_adj_matrix = unpooled_boundary_adj_matrix[:200, :200]
-            #     unpooled_bb_adj_matrix = unpooled_bb_adj_matrix[:, :200]
-            #
-            # unpooled_adj_matrix = np.zeros((x + building_n, x + building_n))
-            # unpooled_adj_matrix[:x, :x] = unpooled_boundary_adj_matrix
-            # unpooled_adj_matrix[x:, x:] = building_adj_matrix
-            # unpooled_adj_matrix[x:, :x] = unpooled_bb_adj_matrix
-            # unpooled_adj_matrix[:x, x:] = unpooled_bb_adj_matrix.T
+            # pooled_adj_matrix = np.zeros((pooled_n_boundary + building_n, pooled_n_boundary + building_n))
+            # pooled_adj_matrix[:pooled_n_boundary, :pooled_n_boundary] = pooled_boundary_adj_matrix
+            # pooled_adj_matrix[pooled_n_boundary:, pooled_n_boundary:] = building_adj_matrix
+            # pooled_adj_matrix[pooled_n_boundary:, :pooled_n_boundary] = pooled_bb_adj_matrix
+            # pooled_adj_matrix[:pooled_n_boundary, pooled_n_boundary:] = pooled_bb_adj_matrix.T
             #
             # import scipy.sparse as sp
-            # sparse_matrix = sp.csr_matrix(unpooled_adj_matrix)
+            # sparse_matrix = sp.csr_matrix(pooled_adj_matrix)
             # coo_matrix = sparse_matrix.tocoo()
             # edge_index = torch.tensor(np.vstack((coo_matrix.row, coo_matrix.col)), dtype=torch.long)
 
-            data = Data(node_features=pooled_node_features,
-                        building_mask=pooled_building_masks, condition=condition,
-                        edge_index=edge_index, num_nodes=pooled_n_boundary + building_n)
+            building_n = torch.sum(building_masks).item()
+            boundary_n = building_masks.shape[0] - building_n
+            unpooled_boundary_n = boundary_n * 2
+
+            if unpooled_boundary_n > 200:
+                x = 200
+            else:
+                x = unpooled_boundary_n
+
+            unpooled_node_features = torch.zeros((x + building_n, 5))
+            for i in range(x):
+                unpooled_node_features[i, 0] += node_features[i // 2, 0]
+                unpooled_node_features[i, 1] += node_features[i // 2, 1]
+                unpooled_node_features[i, 2] += node_features[i // 2, 2]
+                unpooled_node_features[i, 3] += node_features[i // 2, 3]
+                unpooled_node_features[i, 4] += node_features[i // 2, 4]
+
+            for i in range(building_n):
+                if unpooled_boundary_n > 200:
+                    x = 200
+                else:
+                    x = unpooled_boundary_n
+                unpooled_node_features[i + x, 0] += node_features[i + boundary_n, 0]
+                unpooled_node_features[i + x, 1] += node_features[i + boundary_n, 1]
+                unpooled_node_features[i + x, 2] += node_features[i + boundary_n, 2]
+                unpooled_node_features[i + x, 3] += node_features[i + boundary_n, 3]
+                unpooled_node_features[i + x, 4] += node_features[i + boundary_n, 4]
+
+            if unpooled_boundary_n > 200:
+                x = 200
+            else:
+                x = unpooled_boundary_n
+            unpooled_building_masks = torch.zeros((x + building_n, 1), dtype=torch.long)
+            unpooled_building_masks[x:, 0] = 1
+
+            if self.condition_type == 'image' or self.condition_type == 'image_resnet34':
+                condition = torch.tensor(np.array(graph.graph['condition']), dtype=torch.float32)
+                condition = condition.unsqueeze(0)
+                condition = condition.repeat(3, 1, 1)
+            else:
+                condition_graph = graph.graph['condition']
+                condition_edge_index = nx.to_scipy_sparse_matrix(condition_graph).tocoo()
+                condition_edge_index = torch.tensor(np.vstack((condition_edge_index.row, condition_edge_index.col)),
+                                                    dtype=torch.long)
+                condition_street_feature = torch.tensor(
+                    np.array([condition_graph.nodes[node]['chunk_features'] for node in condition_graph.nodes()]),
+                    dtype=torch.float32)
+
+                condition = Data(condition_street_feature=condition_street_feature,
+                                 edge_index=condition_edge_index,
+                                 num_nodes=condition_graph.number_of_nodes())
+
+            # edge_index = nx.to_scipy_sparse_matrix(graph).tocoo()
+            # edge_index = torch.tensor(np.vstack((edge_index.row, edge_index.col)), dtype=torch.long)
+            adj_matrix = nx.to_numpy_array(graph)
+            boundary_adj_matrix = adj_matrix[:boundary_n, :boundary_n]
+            building_adj_matrix = adj_matrix[boundary_n:, boundary_n:]
+            bb_adj_matrix = adj_matrix[boundary_n:, :boundary_n]
+
+            unpooled_n_boundary = (boundary_n + 1) // 2
+            unpooled_boundary_adj_matrix = np.zeros((x, x))
+            for i in range(x):
+                for j in range(x):
+                    unpooled_boundary_adj_matrix[i, j] = boundary_adj_matrix[i // 2, j // 2]
+
+            unpooled_bb_adj_matrix = np.zeros((building_n, x))
+            for i in range(building_n):
+                for j in range(x):
+                    unpooled_bb_adj_matrix[i, j] = bb_adj_matrix[i, j // 2]
+
+            if unpooled_n_boundary > 200:
+                unpooled_boundary_adj_matrix = unpooled_boundary_adj_matrix[:200, :200]
+                unpooled_bb_adj_matrix = unpooled_bb_adj_matrix[:, :200]
+
+            unpooled_adj_matrix = np.zeros((x + building_n, x + building_n))
+            unpooled_adj_matrix[:x, :x] = unpooled_boundary_adj_matrix
+            unpooled_adj_matrix[x:, x:] = building_adj_matrix
+            unpooled_adj_matrix[x:, :x] = unpooled_bb_adj_matrix
+            unpooled_adj_matrix[:x, x:] = unpooled_bb_adj_matrix.T
+
+            import scipy.sparse as sp
+            sparse_matrix = sp.csr_matrix(unpooled_adj_matrix)
+            coo_matrix = sparse_matrix.tocoo()
+            edge_index = torch.tensor(np.vstack((coo_matrix.row, coo_matrix.col)), dtype=torch.long)
+
+            data = Data(node_features=unpooled_node_features,
+                        building_mask=unpooled_building_masks, condition=condition,
+                        edge_index=edge_index, num_nodes=x + building_n)
 
             return data
         else:
